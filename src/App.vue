ccc07d79fc3f073bc162a1fdc99d89537ce98da5
Michael Gecht <gecht.m@gmail.com>
2019-03-10 11:39:35 +0100

    Divide into own components

--- src/App.vue
+++ src/App.vue
@@ -14,57 +14,19 @@
       <span class="title ml-3 mr-5"
         >Google&nbsp;<span class="font-weight-light">Keep</span></span
       >
-      <v-text-field solo-inverted hide-details label="Search"></v-text-field>
-      <v-spacer></v-spacer>
     </v-toolbar>
     <v-content>
-      <v-container fluid pa-0>
-        <v-layout row wrap justify-center>
-          <v-flex xs12>
-            <v-sheet height="100vw">
-              <v-calendar
-                :show-month-on-first="false"
-                ref="calendar"
-                color="primary"
-                :locale="locale"
-                :type="type"
-                v-model="start"
-                :end="end"
-              ></v-calendar>
-            </v-sheet>
-          </v-flex>
-
-          <v-flex sm4 xs12 row wrap>
-            <v-btn icon @click="$refs.calendar.prev()">
-              <v-icon dark left>keyboard_arrow_left</v-icon>
-            </v-btn>
-            <v-spacer></v-spacer>
-            <span>{{ start }}</span>
-            <v-btn icon @click="$refs.calendar.next()">
-              <v-icon dark left>keyboard_arrow_right</v-icon>
-            </v-btn>
-          </v-flex>
-        </v-layout>
-      </v-container>
+      <calendar></calendar>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import Calendar from "@/components/Calendar";
+
 export default {
+  components: { Calendar },
   data: () => ({
-    type: "month",
-    start: "2019-01-01",
-    end: "2019-01-06",
-    typeOptions: [
-      { text: "Day", value: "day" },
-      { text: "4 Day", value: "4day" },
-      { text: "Week", value: "week" },
-      { text: "Month", value: "month" },
-      { text: "Custom Daily", value: "custom-daily" },
-      { text: "Custom Weekly", value: "custom-weekly" }
-    ],
-    locale: "en-us",
     drawer: null
   })
 };
---
+++ src/components/Calendar.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container fluid pa-0>
+    <v-layout row wrap justify-center>
+      <v-flex xs12>
+        <v-sheet height="100vw">
+          <v-calendar
+            :show-month-on-first="false"
+            ref="calendar"
+            color="primary"
+            :locale="locale"
+            :type="type"
+            v-model="start"
+            :end="end"
+          ></v-calendar>
+        </v-sheet>
+      </v-flex>
+
+      <v-flex sm4 xs12 row wrap>
+        <v-btn icon @click="$refs.calendar.prev()">
+          <v-icon dark left>keyboard_arrow_left</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <span>{{ start }}</span>
+        <v-btn icon @click="$refs.calendar.next()">
+          <v-icon dark left>keyboard_arrow_right</v-icon>
+        </v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Calendar",
+  data: () => ({
+    type: "month",
+    start: "2019-01-01",
+    end: "2019-01-06",
+    typeOptions: [
+      { text: "Day", value: "day" },
+      { text: "4 Day", value: "4day" },
+      { text: "Week", value: "week" },
+      { text: "Month", value: "month" },
+      { text: "Custom Daily", value: "custom-daily" },
+      { text: "Custom Weekly", value: "custom-weekly" }
+    ],
+    locale: "en-us"
+  })
+};
+</script>
