24091413d3b0e79b97a6d3e4567790e799eaddfb
Michael Gecht <gecht.m@gmail.com>
2019-03-10 19:47:46 +0100

    Add dummy events to month view

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -13,7 +13,51 @@
             :start="start"
             :end="end"
             :weekdays="weekdays"
-          ></v-calendar>
+          >
+            <template v-slot:day="{ date }">
+              <template v-for="event in eventsMap[date]">
+                <v-menu
+                  :key="event.title"
+                  v-model="event.open"
+                  full-width
+                  offset-x
+                >
+                  <template v-slot:activator="{ on }">
+                    <div
+                      v-if="!event.time"
+                      v-ripple
+                      class="my-event"
+                      v-on="on"
+                      v-html="event.title"
+                    ></div>
+                  </template>
+                  <v-card color="grey lighten-4" min-width="350px" flat>
+                    <v-toolbar color="primary" dark>
+                      <v-btn icon>
+                        <v-icon>edit</v-icon>
+                      </v-btn>
+                      <v-toolbar-title v-html="event.title"></v-toolbar-title>
+                      <v-spacer></v-spacer>
+                      <v-btn icon>
+                        <v-icon>favorite</v-icon>
+                      </v-btn>
+                      <v-btn icon>
+                        <v-icon>more_vert</v-icon>
+                      </v-btn>
+                    </v-toolbar>
+                    <v-card-title primary-title>
+                      <span v-html="event.details"></span>
+                    </v-card-title>
+                    <v-card-actions>
+                      <v-btn flat color="secondary">
+                        Cancel
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-menu>
+              </template>
+            </template>
+          </v-calendar>
         </v-sheet>
       </v-flex>
     </v-layout>
@@ -29,7 +73,57 @@ export default {
   name: "Calendar",
   data: () => ({
     end: "2019-01-06",
-    weekdays: [1, 2, 3, 4, 5, 6, 0]
+    weekdays: [1, 2, 3, 4, 5, 6, 0],
+    events: [
+      {
+        title: "Vacation",
+        details: "Going to the beach!",
+        date: "2018-12-30",
+        open: false
+      },
+      {
+        title: "Vacation",
+        details: "Going to the beach!",
+        date: "2018-12-31",
+        open: false
+      },
+      {
+        title: "Vacation",
+        details: "Going to the beach!",
+        date: "2019-01-01",
+        open: false
+      },
+      {
+        title: "Meeting",
+        details: "Spending time on how we do not have enough time",
+        date: "2019-01-07",
+        open: false
+      },
+      {
+        title: "30th Birthday",
+        details: "Celebrate responsibly",
+        date: "2019-01-03",
+        open: false
+      },
+      {
+        title: "New Year",
+        details: "Eat chocolate until you pass out",
+        date: "2019-01-01",
+        open: false
+      },
+      {
+        title: "Conference",
+        details: "Mute myself the whole time and wonder why I am on this call",
+        date: "2019-01-21",
+        open: false
+      },
+      {
+        title: "Hackathon",
+        details: "Code like there is no tommorrow",
+        date: "2019-02-01",
+        open: false
+      }
+    ]
   }),
   props: {
     start: {
@@ -42,6 +136,11 @@ export default {
     }
   },
   computed: {
+    eventsMap() {
+      const map = {};
+      this.events.forEach(e => (map[e.date] = map[e.date] || []).push(e));
+      return map;
+    },
     ...mapGetters(["locale"])
   },
   methods: {
@@ -59,3 +158,20 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.my-event {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  border-radius: 2px;
+  background-color: #1867c0;
+  color: #ffffff;
+  border: 1px solid #1867c0;
+  width: 100%;
+  font-size: 12px;
+  padding: 3px;
+  cursor: pointer;
+  margin-bottom: 1px;
+}
+</style>
