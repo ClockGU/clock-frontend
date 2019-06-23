b6dea37a85782870aeea46f7ddd05d4e3e239912
Michael Gecht <gecht.m@gmail.com>
2019-06-23 18:49:31 +0200

    Update ContractList visualization

--- src/views/ContractList.vue
+++ src/views/ContractList.vue
@@ -1,13 +1,35 @@
 <template>
-  <v-container>
-    <ul>
-      <li v-for="contract in contracts" :key="contract.uuid">
-        <v-btn
-          :to="{ name: 'editContract', params: { uuid: contract.uuid } }"
-          >{{ contract.name }}</v-btn
-        >
-      </li>
-    </ul>
+  <v-container fluid grid-list-lg>
+    <v-layout row wrap>
+      <template v-for="contract in contracts">
+        <v-flex xs6 :key="contract.uuid">
+          <v-card color="blue-grey darken-2" class="white--text">
+            <v-card-title primary-title>
+              <div class="headline">{{ contract.name }}</div>
+            </v-card-title>
+            <v-card-text>
+              <p>
+                Start: {{ contract.date.start | toDate }}
+                <br />
+                End: {{ contract.date.end | toDate }}
+                <br />
+                Hours: {{ contract.hours }}
+              </p>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                flat
+                dark
+                :to="{ name: 'editContract', params: { uuid: contract.uuid } }"
+                >Edit</v-btn
+              >
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </template>
+    </v-layout>
+
     <portal to="fab">
       <v-btn
         absolute
@@ -26,6 +48,7 @@
 
 <script>
 import { mapState } from "vuex";
+import { format } from "date-fns";
 
 export default {
   name: "ContractList",
@@ -33,6 +56,11 @@ export default {
     ...mapState({
       contracts: state => state.contract.contracts
     })
+  },
+  filters: {
+    toDate(date) {
+      return format(date, "YYYY-MM-DD");
+    }
   }
 };
 </script>
