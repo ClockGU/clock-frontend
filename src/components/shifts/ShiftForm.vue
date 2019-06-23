1b7e142bd196582d4ed99ebaa5b9c2db718b6c3f
Michael Gecht <gecht.m@gmail.com>
2019-06-23 17:36:12 +0200

    Use actual Contracts in ShiftForm

--- src/components/shifts/ShiftForm.vue
+++ src/components/shifts/ShiftForm.vue
@@ -14,27 +14,37 @@
               ></v-select>
             </v-flex>
             <v-flex xs12 md5>
-              <shift-form-date-time-input v-model="shift.date" :shift="shift" type="start"/>
+              <shift-form-date-time-input
+                v-model="shift.date"
+                :shift="shift"
+                type="start"
+              />
             </v-flex>
             <v-flex xs1>to</v-flex>
             <v-flex xs12 md5>
-              <shift-form-date-time-input v-model="shift.date" :shift="shift" type="end"/>
+              <shift-form-date-time-input
+                v-model="shift.date"
+                :shift="shift"
+                type="end"
+              />
             </v-flex>
             <v-flex xs12 md7>
-              <shift-form-select v-model="shift.type"/>
+              <shift-form-select v-model="shift.type" />
             </v-flex>
             <v-flex xs12 md7>
-              <shift-form-tags v-model="shift.tags"/>
+              <shift-form-tags v-model="shift.tags" />
             </v-flex>
             <v-flex xs12 md7>
-              <shift-form-input v-model="shift.note"/>
+              <shift-form-input v-model="shift.note" />
             </v-flex>
           </v-layout>
         </v-card-text>
         <v-card-actions>
           <v-btn flat @click="destroy(destroy)">Delete</v-btn>
           <v-spacer></v-spacer>
-          <v-btn flat @click="submit({ create: create, update: update })">{{ saveLabel }}</v-btn>
+          <v-btn flat @click="submit({ create: create, update: update })">{{
+            saveLabel
+          }}</v-btn>
         </v-card-actions>
       </v-card>
     </template>
@@ -48,6 +58,8 @@ import ShiftFormSelect from "@/components/shifts/ShiftFormSelect";
 import ShiftFormInput from "@/components/shifts/ShiftFormInput";
 import ShiftFormTags from "@/components/shifts/ShiftFormTags";
 
+import { mapState } from "vuex";
+
 export default {
   name: "ShiftForm",
   components: {
@@ -58,8 +70,7 @@ export default {
     ShiftFormTags
   },
   data: () => ({
-    select: null,
-    contracts: [{ uuid: "1234", name: "Starker Start" }]
+    select: null
   }),
   props: {
     uuid: {
@@ -68,6 +79,9 @@ export default {
     }
   },
   computed: {
+    ...mapState("contract", {
+      contracts: state => state.contracts
+    }),
     saveLabel() {
       return this.uuid === null ? "Save" : "Update";
     }
