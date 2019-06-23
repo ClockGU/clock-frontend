05977fa2c7cedfd5fb0407b44718bf47c2457114
Michael Gecht <gecht.m@gmail.com>
2019-06-23 16:52:03 +0200

    Add Contract selection input

--- src/components/shifts/ShiftForm.vue
+++ src/components/shifts/ShiftForm.vue
@@ -4,38 +4,37 @@
       <v-card>
         <v-card-text>
           <v-layout row wrap align-center>
+            <v-flex xs12>
+              <v-select
+                v-model="shift.contract"
+                :items="contracts"
+                item-text="name"
+                item-value="uuid"
+                label="Please select a contract"
+              ></v-select>
+            </v-flex>
             <v-flex xs12 md5>
-              <shift-form-date-time-input
-                v-model="shift.date"
-                :shift="shift"
-                type="start"
-              />
+              <shift-form-date-time-input v-model="shift.date" :shift="shift" type="start"/>
             </v-flex>
             <v-flex xs1>to</v-flex>
             <v-flex xs12 md5>
-              <shift-form-date-time-input
-                v-model="shift.date"
-                :shift="shift"
-                type="end"
-              />
+              <shift-form-date-time-input v-model="shift.date" :shift="shift" type="end"/>
             </v-flex>
             <v-flex xs12 md7>
-              <shift-form-select v-model="shift.type" />
+              <shift-form-select v-model="shift.type"/>
             </v-flex>
             <v-flex xs12 md7>
-              <shift-form-tags v-model="shift.tags" />
+              <shift-form-tags v-model="shift.tags"/>
             </v-flex>
             <v-flex xs12 md7>
-              <shift-form-input v-model="shift.note" />
+              <shift-form-input v-model="shift.note"/>
             </v-flex>
           </v-layout>
         </v-card-text>
         <v-card-actions>
           <v-btn flat @click="destroy(destroy)">Delete</v-btn>
           <v-spacer></v-spacer>
-          <v-btn flat @click="submit({ create: create, update: update })">{{
-            saveLabel
-          }}</v-btn>
+          <v-btn flat @click="submit({ create: create, update: update })">{{ saveLabel }}</v-btn>
         </v-card-actions>
       </v-card>
     </template>
@@ -58,6 +57,10 @@ export default {
     ShiftFormSelect,
     ShiftFormTags
   },
+  data: () => ({
+    select: null,
+    contracts: [{ uuid: "1234", name: "Starker Start" }]
+  }),
   props: {
     uuid: {
       type: String,
@@ -73,12 +76,12 @@ export default {
     submit(callback) {
       this.uuid === null ? callback.create() : callback.update();
 
-      this.$router.push({ name: "shiftList" });
+      this.$router.push({ name: "c" });
     },
     destroy(callback) {
       callback();
 
-      this.$router.push({ name: "shiftList" });
+      this.$router.push({ name: "c" });
     }
   }
 };
