8e2a5d229937ba78b9dc9df9a4b6e625534c16c6
Michael Gecht <gecht.m@gmail.com>
2019-06-23 16:51:38 +0200

    Add new contract button

--- src/views/ContractList.vue
+++ src/views/ContractList.vue
@@ -2,10 +2,21 @@
   <v-layout>
     <ul>
       <li v-for="contract in contracts" :key="contract.uuid">
-        <v-btn :to="{ name: 'editContract', params: { uuid: contract.uuid } }">{{ contract.name }}</v-btn>
+        <v-btn
+          :to="{ name: 'editContract', params: { uuid: contract.uuid } }"
+          >{{ contract.name }}</v-btn
+        >
       </li>
     </ul>
-    <v-btn absolute dark fab top right color="pink" :to="{ name: 'createContract' }">
+    <v-btn
+      absolute
+      dark
+      fab
+      top
+      right
+      color="pink"
+      :to="{ name: 'createContract' }"
+    >
       <v-icon>add</v-icon>
     </v-btn>
   </v-layout>
