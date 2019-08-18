4f5adf28003aa3fdc1588695ec8ba4eaa5a0563a
Michael Gecht <gecht.m@gmail.com>
2019-08-18 12:25:37 +0200

    chore: upgrade eslintrc and jest.config.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,12 @@ module.exports = {
   env: {
     node: true
   },
+  plugins: ["vuetify"],
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vuetify/no-deprecated-classes": "error"
   },
   parserOptions: {
     parser: "babel-eslint"
