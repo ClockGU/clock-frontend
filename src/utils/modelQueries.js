5a1a291db49b8ee56509246bc7cb6fd6fda880f5
Michael Gecht <gecht.m@gmail.com>
2019-08-28 07:28:08 +0200

    refactor: mappingFunctions into object

--- src/utils/modelQueries.js
+++ src/utils/modelQueries.js
@@ -1,36 +1,37 @@
 import { Contract } from "@/models/Contracts";
 import { Shift } from "@/models/Shifts";
 
-export function mapContracts(data) {
-  const contract = new Contract({
-    uuid: data.id,
-    name: data.name,
-    date: { start: data.start_date, end: data.end_date },
-    hours: data.hours
-  });
+export const mappingFunctions = {
+  mapContracts: data => {
+    const contract = new Contract({
+      uuid: data.id,
+      name: data.name,
+      date: { start: data.start_date, end: data.end_date },
+      hours: data.hours
+    });
 
-  return contract;
-}
+    return contract;
+  },
+  mapShifts: data => {
+    const types = {
+      st: { text: "Shift", value: "st" },
+      sk: { text: "Sick", value: "sk" },
+      vn: { text: "Vacation", value: "vn" }
+    };
 
-export function mapShifts(data) {
-  const types = {
-    st: { text: "Shift", value: "st" },
-    sk: { text: "Sick", value: "sk" },
-    vn: { text: "Vacation", value: "vn" }
-  };
+    const shift = new Shift({
+      uuid: data.id,
+      name: data.name,
+      date: { start: data.started, end: data.stopped },
+      contract: data.contract,
+      type: types[data.type],
+      note: data.note,
+      tags: data.tags
+    });
 
-  const shift = new Shift({
-    uuid: data.id,
-    name: data.name,
-    date: { start: data.started, end: data.stopped },
-    contract: data.contract,
-    type: types[data.type],
-    note: data.note,
-    tags: data.tags
-  });
-
-  return shift;
-}
+    return shift;
+  }
+};
 
 export function mapItemsAndListsToModel(data, fn) {
   let output;
