509d7205161f1894997954bffe009213ca840c09
Michael Gecht <gecht.m@gmail.com>
2019-08-29 19:29:46 +0200

    style: apply some style to login form

--- src/views/ViewLogin.vue
+++ src/views/ViewLogin.vue
@@ -1,35 +1,51 @@
 <template>
-  <v-container>
-    <v-layout wrap justify-center>
-      <v-flex xs12 md8 lg10>
-        <v-text-field
-          v-model="username"
-          label="Username"
-          @keyup.enter="submit"
-        ></v-text-field>
-        <v-text-field
-          v-model="password"
-          label="Password"
-          type="password"
-          @keyup.enter="submit"
-        ></v-text-field>
-        <v-btn @click="submit">Login</v-btn>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-row align="center" justify="center">
+    <v-col cols="12" sm="8" md="4">
+      <v-card class="elevation-12">
+        <v-toolbar color="yellow darken-3" dark flat>
+          <v-toolbar-title>Login form</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              v-model="email"
+              label="E-Mail"
+              prepend-icon="person"
+              type="text"
+              @keyup.enter="submit"
+            ></v-text-field>
+
+            <v-text-field
+              id="password"
+              v-model="password"
+              label="Password"
+              name="password"
+              prepend-icon="lock"
+              type="password"
+              @keyup.enter="submit"
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="yellow darken-3">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
   name: "ViewLogin",
   data: () => ({
-    username: null,
+    email: null,
     password: null
   }),
   methods: {
     submit() {
       this.$store.dispatch("auth/login", {
-        email: this.username,
+        email: this.email,
         password: this.password
       });
     }
