2d66902865e548324197f25c50d2cdf5a6ed1c20
Michael Gecht <gecht.m@gmail.com>
2019-08-30 08:19:37 +0200

    tests: separate existing tests

--- tests/unit/models/Contracts.spec.js
+++ tests/unit/models/Contracts.spec.js
@@ -1,58 +1,5 @@
-import { defaultContractDate } from "@/utils/date";
 import { Contract } from "@/models/ContractModel";
 import { addDays } from "date-fns";
-
-describe("Contracts/defaultContractDate()", () => {
-  // Winter term tests
-  it("returns correct start date for winter term given the starting edge", () => {
-    expect(
-      defaultContractDate({ type: "start", date: new Date(2018, 9, 1) })
-    ).toEqual(new Date(2018, 9, 1));
-  });
-
-  it("returns correct start date for winter term", () => {
-    expect(
-      defaultContractDate({ type: "start", date: new Date(2019, 1, 1) })
-    ).toEqual(new Date(2018, 9, 1));
-  });
-
-  it("returns correct end date for winter term given the ending edge", () => {
-    expect(
-      defaultContractDate({ type: "end", date: new Date(2019, 2, 31) })
-    ).toEqual(new Date(2019, 2, 31));
-  });
-
-  it("returns correct end date for winter term", () => {
-    expect(
-      defaultContractDate({ type: "end", date: new Date(2019, 1, 1) })
-    ).toEqual(new Date(2019, 2, 31));
-  });
-
-  // Summer term tests
-  it("returns correct start date for summer term given the starting edge", () => {
-    expect(
-      defaultContractDate({ type: "start", date: new Date(2018, 3, 1) })
-    ).toEqual(new Date(2018, 3, 1));
-  });
-
-  it("returns correct start date for summer term", () => {
-    expect(
-      defaultContractDate({ type: "start", date: new Date(2019, 5, 1) })
-    ).toEqual(new Date(2019, 3, 1));
-  });
-
-  it("returns correct end date for summer term given the ending edge", () => {
-    expect(
-      defaultContractDate({ type: "end", date: new Date(2019, 8, 30) })
-    ).toEqual(new Date(2019, 8, 30));
-  });
-
-  it("returns correct end date for summer term", () => {
-    expect(
-      defaultContractDate({ type: "end", date: new Date(2019, 5, 1) })
-    ).toEqual(new Date(2019, 8, 30));
-  });
-});
 
 describe("Contracts.js", () => {
   const date = {
