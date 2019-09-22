07cdb3cdb5db9eab572c26a9170d7a7bc9a76992
Michael Gecht <gecht.m@gmail.com>
2019-09-22 13:16:03 +0200

    refactor(ViewLogin): remove v-banner

--- src/views/ViewLogin.vue
+++ src/views/ViewLogin.vue
@@ -1,20 +1,6 @@
 <template>
   <v-row align="center" justify="center">
     <v-col cols="12" sm="8" md="7">
-      <v-fade-transition>
-        <v-banner v-if="loginError" elevation="6"
-          >The e-mail and/or password you entered is not valid.
-
-          <template v-slot:actions="{ dismiss }">
-            <v-btn icon text color="deep-purple accent-4" @click="dismiss">
-              <v-icon>close</v-icon>
-            </v-btn>
-          </template>
-        </v-banner>
-      </v-fade-transition>
-    </v-col>
-
-    <v-col cols="12" sm="8" md="7">
       <LoginForm />
     </v-col>
   </v-row>
@@ -25,11 +11,6 @@ import LoginForm from "@/components/LoginForm";
 
 export default {
   name: "ViewLogin",
-  components: { LoginForm },
-  computed: {
-    loginError() {
-      return !!this.$store.state.auth.authenticationError;
-    }
-  }
+  components: { LoginForm }
 };
 </script>
