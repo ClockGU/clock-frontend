6a62f9aa9e4b11778a9e2d94246399c9a74169d9
Michael Gecht <gecht.m@gmail.com>
2019-09-30 15:00:02 +0200

    feat(store): persist vuex state in localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import { getField, updateField } from "vuex-map-fields";
-// import createPersistedState from "vuex-persistedstate";
+import createPersistedState from "vuex-persistedstate";
 
 import auth from "@/store/modules/auth";
 // import calendar from "@/store/modules/calendar";
@@ -70,6 +70,6 @@ export default new Vuex.Store({
     shift,
     contract,
     snackbar
-  }
-  // plugins: [createPersistedState()]
+  },
+  plugins: [createPersistedState()]
 });
