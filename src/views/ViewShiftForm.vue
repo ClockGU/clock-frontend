4851785480f0b92ccb9029ef7d4c377a0edb43a0
Michael Gecht <gecht.m@gmail.com>
2019-10-20 11:30:16 +0200

    refactor(ShiftForm): put FrameApi into view, instead of ShiftForm

--- src/views/ViewShiftForm.vue
+++ src/views/ViewShiftForm.vue
@@ -1,29 +1,68 @@
 <template>
-  <ShiftForm v-if="entity" :uuid="uuid" :entity="entity" />
+  <v-container>
+    <FrameApi
+      v-if="endpoint"
+      v-slot="{ methods: { query }, status: { error, loading } }"
+      :endpoint="endpoint"
+      @success="redirect"
+    >
+      <ShiftForm v-if="entity" :query="query" :uuid="uuid" :entity="entity" />
+    </FrameApi>
+
+    <ShiftForm
+      v-if="endpoint === null && entity"
+      :query="query"
+      :uuid="uuid"
+      :entity="entity"
+    />
+  </v-container>
 </template>
 
 <script>
 import ShiftForm from "@/components/shifts/ShiftForm";
 import { Shift } from "@/models/ShiftModel";
 import ShiftService from "@/services/shift.service";
+import FrameApi from "@/components/FrameApi";
 
 export default {
-  name: "ViewCreateShift",
-  components: { ShiftForm },
+  name: "ViewShiftForm",
+  components: { FrameApi, ShiftForm },
   props: {
     uuid: {
       type: String,
       default: null
+    },
+    pseudo: {
+      type: Boolean,
+      default: false
+    },
+    routerPush: {
+      type: Object,
+      default: () => ({ name: "c" })
     }
   },
   data() {
     return {
-      entity: null
+      entity: null,
+      endpoint: null
     };
   },
   async created() {
     const shifts = this.$store.state.shift.shifts;
-    const entity = shifts.find(shift => shift.uuid === this.uuid);
+
+    let entity;
+    if (this.pseudo) {
+      entity = this.$store.state.shift.pseudoShifts.find(
+        shift => shift.uuid === this.uuid
+      );
+      this.query = this.updatePseudoShift;
+    } else {
+      entity = shifts.find(shift => shift.uuid === this.uuid);
+      this.endpoint = data =>
+        this.uuid === null
+          ? ShiftService.create(data.toPayload())
+          : ShiftService.update(data.toPayload(), this.uuid);
+    }
 
     if (entity !== undefined) {
       this.entity = new Shift(entity);
@@ -32,6 +71,25 @@ export default {
       this.entity = new Shift(response.data);
     } else {
       this.entity = new Shift();
+    }
+  },
+  methods: {
+    updatePseudoShift(shift) {
+      this.$store.dispatch("shift/updatePseudoShift", shift);
+    },
+    redirect({ data }) {
+      console.log(data);
+      return new Promise((resolve, reject) => {
+        data
+          .then(() => {
+            this.$router.push({ name: "reviewShifts" });
+
+            resolve();
+          })
+          .catch(error => {
+            reject(error);
+          });
+      });
     }
   }
 };
