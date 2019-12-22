0a7cfcf3f220ac57d9fa01d229e200c9de98a7f9
Michael Gecht <gecht.m@gmail.com>
2019-12-22 17:42:57 +0100

    project(cypress): add clockinout_split.spec.js

--- src/components/ClockedShiftSplitWarning.vue
+++ src/components/ClockedShiftSplitWarning.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card>
+  <v-card data-cy="clicked-shift-split-warning">
     <v-card-title class="headline word-break">
       Shift overflow
     </v-card-title>
@@ -13,6 +13,7 @@
         <v-col sm="10" class="pb-0">
           <v-select
             v-model="selected"
+            data-cy="select"
             :items="options"
             label="Select an action"
             hide-details
@@ -23,6 +24,7 @@
         </v-col>
         <v-col sm="2">
           <v-btn
+            data-cy="reset"
             text
             x-large
             :disabled="persistPseudoShiftToVuex"
@@ -34,15 +36,15 @@
       </v-row>
     </v-card-text>
 
-    <v-list>
+    <v-list data-cy="overflowing-shift">
       <v-subheader>Overflowing shift</v-subheader>
       <v-list-item two-line>
         <v-list-item-content>
-          <v-list-item-title>
+          <v-list-item-title data-cy="overflowing-shift-duration">
             {{ shift.representationalDuration("hm") }}
           </v-list-item-title>
 
-          <v-list-item-subtitle>
+          <v-list-item-subtitle data-cy="overflowing-shift-dates">
             {{ shift.start | formatDate }} -
             {{ shift.end | formatDate }}
           </v-list-item-subtitle>
@@ -52,20 +54,24 @@
 
     <v-divider></v-divider>
 
-    <v-list v-if="shifts">
+    <v-list v-if="shifts" data-cy="new-shifts">
       <v-subheader>New shifts</v-subheader>
-      <v-list-item v-for="newShift in shifts" :key="newShift.uuid" two-line>
+      <v-list-item
+        v-for="(newShift, i) in shifts"
+        :key="newShift.uuid"
+        two-line
+      >
         <v-list-item-content>
-          <v-list-item-title>
+          <v-list-item-title :data-cy="'new-shift-duration-' + i">
             {{ newShift.representationalDuration("hm") }}
           </v-list-item-title>
-          <v-list-item-subtitle>
+          <v-list-item-subtitle :data-cy="'new-shift-dates-' + i">
             {{ newShift.start | formatDate }} -
             {{ newShift.end | formatDate }}
           </v-list-item-subtitle>
         </v-list-item-content>
 
-        <v-list-item-action>
+        <v-list-item-action :data-cy="'options-' + i">
           <v-menu offset-y>
             <template v-slot:activator="{ on }">
               <v-btn icon v-on="on">
@@ -76,10 +82,13 @@
             </template>
 
             <v-list>
-              <v-list-item @click="editShift(newShift)">
+              <v-list-item :data-cy="'edit-' + i" @click="editShift(newShift)">
                 <v-list-item-title>Edit</v-list-item-title>
               </v-list-item>
-              <v-list-item @click="deleteShift(newShift)">
+              <v-list-item
+                :data-cy="'delete-' + i"
+                @click="deleteShift(newShift)"
+              >
                 <v-list-item-title>Delete</v-list-item-title>
               </v-list-item>
             </v-list>
@@ -100,17 +109,23 @@
     </v-list>
 
     <v-card-actions>
-      <v-btn :disabled="!selected" text color="primary" @click="submit">
+      <v-btn
+        data-cy="save"
+        :disabled="!selected"
+        text
+        color="primary"
+        @click="submit"
+      >
         Save
       </v-btn>
       <v-dialog v-model="dialogReset" max-width="350">
         <template v-slot:activator="{ on }">
-          <v-btn :disabled="!selected" text v-on="on">
+          <v-btn data-cy="discard" :disabled="!selected" text v-on="on">
             Discard
           </v-btn>
         </template>
 
-        <v-card>
+        <v-card data-cy="discard-dialog">
           <v-card-title class="headline" primary-title>
             Discard shift
           </v-card-title>
@@ -122,8 +137,10 @@
           <v-divider></v-divider>
 
           <v-card-actions>
-            <v-btn color="error" text @click="reset">Confirm</v-btn>
-            <v-btn text @click="dialogReset = false">
+            <v-btn data-cy="dialog-confirm" color="error" text @click="reset">
+              Confirm
+            </v-btn>
+            <v-btn data-cy="dialog-cancel" text @click="dialogReset = false">
               Cancel
             </v-btn>
           </v-card-actions>
@@ -173,8 +190,8 @@ export default {
     options: [
       { text: "Keep both shifts and split across days", value: "both" },
       { text: "Keep first", value: "first" },
-      { text: "Keep second", value: "second" },
-      { text: "Discard both", value: "none" }
+      { text: "Keep second", value: "second" }
+      // { text: "Discard both", value: "none" }
     ],
     dialogReset: false
   }),
@@ -224,7 +241,9 @@ export default {
     }
   },
   created() {
-    if (!this.persistPseudoShiftToVuex) this.setPseudoShifts();
+    this.setPseudoShifts();
+    // TODO: What edgecase was this check for?
+    // if (!this.persistPseudoShiftToVuex) this.setPseudoShifts();
   },
   methods: {
     deleteShift(shift) {
---
+++ tests/e2e/specs/clockinout/clockinout_split.spec.js
@@ -0,0 +1,205 @@
+describe("when a shift overflows into the next day", () => {
+  beforeEach(() => {
+    const time = new Date(2019, 10, 20, 10).getTime();
+    cy.clock(time, ["Date"]);
+    cy.server();
+    cy.route("GET", "/api/shifts/", "fixture:shifts.json");
+    cy.route("GET", "/api/contracts/", "fixture:contracts.json");
+    cy.route("GET", "/api/clockedinshifts/", "fixture:clockin_overflow.json");
+
+    cy.login();
+    cy.selectContract();
+
+    cy.visit("http://localhost:8080");
+
+    cy.tick(1000);
+    cy.wait(500);
+    cy.get("[data-cy=clock-in-out-button]").click();
+  });
+
+  it("shows the full screen review dialog", () => {
+    cy.get("[data-cy=review-shift]").should("be.visible");
+  });
+
+  it("shows the overflowing shift", () => {
+    cy.get("[data-cy=overflowing-shift-duration]").should("contain", "19h 00m");
+    cy.get("[data-cy=overflowing-shift-dates]").should(
+      "contain",
+      "2019-11-19 15:00 - 2019-11-20 10:00"
+    );
+  });
+
+  it("shows the new pseudo shifts", () => {
+    cy.get("[data-cy=new-shift-duration-0]").should("contain", "08h 59m");
+    cy.get("[data-cy=new-shift-dates-0]").should(
+      "contain",
+      "2019-11-19 15:00 - 2019-11-19 23:59"
+    );
+
+    cy.get("[data-cy=new-shift-duration-1]").should("contain", "10h 00m");
+    cy.get("[data-cy=new-shift-dates-1]").should(
+      "contain",
+      "2019-11-20 00:00 - 2019-11-20 10:00"
+    );
+  });
+
+  it("shows a select element and disabled reset button", () => {
+    cy.get("[data-cy=select]").should("be.enabled");
+    cy.get("[data-cy=reset]").should("be.disabled");
+  });
+
+  it("shows save and discard buttons", () => {
+    cy.get("[data-cy=save]").should("be.enabled");
+    cy.get("[data-cy=discard]").should("be.enabled");
+  });
+
+  it("can save the pseudo shifts", () => {
+    cy.server();
+    cy.route("POST", "/api/shifts/", {});
+    cy.route("DELETE", "/api/shifts/cf9c16d8-5965-4c06-947b-7e860922eafb/", {});
+    cy.get("[data-cy=save]").click();
+    cy.get("[data-cy=review-shift]").should("not.be.visible");
+  });
+});
+
+describe("when the user wants to update the pseudo shifts", () => {
+  beforeEach(() => {
+    const time = new Date(2019, 10, 20, 10).getTime();
+    cy.clock(time, ["Date"]);
+    cy.server();
+    cy.route("GET", "/api/shifts/", "fixture:shifts.json");
+    cy.route("GET", "/api/contracts/", "fixture:contracts.json");
+    cy.route("GET", "/api/clockedinshifts/", "fixture:clockin_overflow.json");
+
+    cy.login();
+    cy.selectContract();
+
+    cy.visit("http://localhost:8080");
+
+    cy.tick(1000);
+    cy.wait(500);
+    cy.get("[data-cy=clock-in-out-button]").click();
+  });
+
+  it("shows another dialog with the ShiftForm", () => {
+    cy.get("[data-cy=options-0]").click();
+    cy.wait(500);
+    cy.get("[data-cy=edit-0]").click();
+    cy.get("[data-cy=review-edit-shift]").should("be.visible");
+  });
+
+  it("delete one shift", () => {
+    cy.get("[data-cy=options-0] > .v-btn").click();
+    cy.wait(500);
+
+    cy.get("[data-cy=delete-0]").click();
+
+    cy.get("[data-cy=new-shift-duration-0]").should("contain", "10h 00m");
+    cy.get("[data-cy=new-shift-dates-0]").should(
+      "contain",
+      "2019-11-20 00:00 - 2019-11-20 10:00"
+    );
+  });
+
+  it("will automatically restore if no shifts are left", () => {
+    cy.get("[data-cy=options-1] > .v-btn").click();
+    cy.wait(500);
+    cy.get("[data-cy=delete-1]").click();
+
+    cy.get("[data-cy=options-0]").click();
+    cy.wait(500);
+    cy.get("[data-cy=delete-0]").click();
+  });
+
+  it("can keep the first only", () => {
+    cy.get("div")
+      .contains("Keep both shifts and split across days")
+      .click();
+
+    cy.get("div")
+      .contains("Keep first")
+      .click();
+
+    cy.get("[data-cy=new-shift-duration-0]").should("contain", "08h 59m");
+    cy.get("[data-cy=new-shift-dates-0]").should(
+      "contain",
+      "2019-11-19 15:00 - 2019-11-19 23:59"
+    );
+
+    cy.get("[data-cy=new-shift-duration-1]").should("not.be.visible");
+  });
+
+  it("can keep the second only", () => {
+    cy.get("div")
+      .contains("Keep both shifts and split across days")
+      .click();
+
+    cy.get("div")
+      .contains("Keep second")
+      .click();
+
+    cy.get("[data-cy=new-shift-duration-0]").should("contain", "10h 00m");
+    cy.get("[data-cy=new-shift-dates-0]").should(
+      "contain",
+      "2019-11-20 00:00 - 2019-11-20 10:00"
+    );
+    cy.get("[data-cy=new-shift-duration-1]").should("not.be.visible");
+  });
+
+  it("can update the time", () => {
+    cy.get("[data-cy=options-0]").click();
+    cy.wait(500);
+    cy.get("[data-cy=edit-0]").click();
+    cy.get("[data-cy=end]")
+      .clear()
+      .type("18:00");
+    cy.get("[data-cy=shift-save]").click();
+  });
+});
+
+describe("when a user wants to discard an overflowing shift", () => {
+  beforeEach(() => {
+    const time = new Date(2019, 10, 20, 10).getTime();
+    cy.clock(time, ["Date"]);
+    cy.server();
+    cy.route("GET", "/api/shifts/", "fixture:shifts.json");
+    cy.route("GET", "/api/contracts/", "fixture:contracts.json");
+    cy.route("GET", "/api/clockedinshifts/", "fixture:clockin_overflow.json");
+
+    cy.login();
+    cy.selectContract();
+
+    cy.visit("http://localhost:8080");
+
+    cy.tick(1000);
+    cy.wait(500);
+    cy.get("[data-cy=clock-in-out-button]").click();
+  });
+
+  it("shows a confirm dialog", () => {
+    cy.get("[data-cy=discard]").click();
+    cy.wait(500);
+    cy.get("[data-cy=review-shift]").should("not.be.visible");
+    cy.get("[data-cy=discard-dialog]").should("be.visible");
+  });
+
+  it("can cancel the discard", () => {
+    cy.get("[data-cy=discard]").click();
+    cy.get("[data-cy=dialog-cancel]").click();
+    cy.get("[data-cy=discard-dialog]").should("not.be.visible");
+    cy.get("[data-cy=review-shift]").should("be.visible");
+  });
+
+  it("can confirm the discard", () => {
+    cy.server();
+    cy.route(
+      "DELETE",
+      "/api/clockedinshifts/cf9c16d8-5965-4c06-947b-7e860922eafb/",
+      {}
+    );
+    cy.get("[data-cy=discard]").click();
+    cy.get("[data-cy=dialog-confirm]").click();
+    cy.get("[data-cy=review-shift]").should("not.be.visible");
+    cy.get("[data-cy=discard-dialog]").should("not.be.visible");
+  });
+});
