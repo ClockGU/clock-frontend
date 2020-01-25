99d2d4464177a3dfa466a030a6f5a4fb82d9dbd3
Michael Gecht <gecht.m@gmail.com>
2020-01-25 14:18:36 +0100

    tests(jwt): add unit test for parseJwt

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -5,7 +5,8 @@ export const parseJwt = token => {
   const base64Url = token.split(".")[1];
   const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
   const jsonPayload = decodeURIComponent(
-    atob(base64)
+    Buffer.from(base64, "base64")
+      .toString()
       .split("")
       .map(c => {
         return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
