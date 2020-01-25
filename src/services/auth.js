70d0d91416f35739f4bad979e56307418729926d
Michael Gecht <gecht.m@gmail.com>
2020-01-25 13:59:33 +0100

    refactor(jwt): extract parseJwt into utils

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,23 +1,5 @@
 import ApiService from "@/services/api";
 
-export function parseJwt(token) {
-  /* Decode JWT token.
-  Source: https://stackoverflow.com/a/38552302
-  */
-  var base64Url = token.split(".")[1];
-  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-  var jsonPayload = decodeURIComponent(
-    atob(base64)
-      .split("")
-      .map(function(c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join("")
-  );
-
-  return JSON.parse(jsonPayload);
-}
-
 const AuthService = {
   /**
    * Login using the provided credentials.
