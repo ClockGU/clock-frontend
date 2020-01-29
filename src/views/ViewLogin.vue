9ebc1924b9462709e266a0de9012cd4b25f4bba9
Michael Gecht <gecht.m@gmail.com>
2020-01-29 09:07:44 +0100

    refactor(LoginForm): adjust style

--- src/views/ViewLogin.vue
+++ src/views/ViewLogin.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-row align="center" justify="center">
-    <v-col cols="12" sm="8" md="7">
+  <v-row :align="$vuetify.breakpoint.mdAndUp ? 'center' : ''" justify="center">
+    <v-col cols="12" sm="8" md="7" class="py-0">
       <LoginForm />
     </v-col>
   </v-row>
