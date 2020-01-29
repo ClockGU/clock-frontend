dc346f7e1f88029af4e8ae40e6aef67f6debbd1b
Michael Gecht <gecht.m@gmail.com>
2020-01-29 22:18:57 +0100

    refactor(ViewContractList): adjust style

--- src/views/ViewContractList.vue
+++ src/views/ViewContractList.vue
@@ -1,57 +1,70 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col>
-        <h1>
-          <template v-if="!editMode">
-            Select a contract
-          </template>
-          <template v-else>
-            Contracts
-          </template>
-        </h1>
-      </v-col>
-    </v-row>
+  <v-row
+    :align="$vuetify.breakpoint.mdAndUp ? 'center' : ''"
+    :justify="$vuetify.breakpoint.mdAndUp ? 'center' : ''"
+  >
+    <v-col cols="12" md="6" class="py-0">
+      <v-card :elevation="$vuetify.breakpoint.smAndDown ? 0 : null">
+        <portal-target name="card-toolbar"></portal-target>
 
-    <v-row v-if="loading" data-cy="contract-list">
-      <v-col cols="12" sm="6" md="4">
-        <v-skeleton-loader
-          data-cy="skeleton"
-          type="card"
-          max-width="350"
-          :loading="true"
-        ></v-skeleton-loader>
-      </v-col>
-    </v-row>
+        <portal
+          :to="$vuetify.breakpoint.smAndDown ? 'app-bar' : 'card-toolbar'"
+        >
+          <v-toolbar slot-scope="{ action }" :elevation="0">
+            <v-app-bar-nav-icon
+              v-if="$vuetify.breakpoint.smAndDown"
+              icon
+              @click="action"
+            ></v-app-bar-nav-icon>
 
-    <v-row v-else>
-      <template v-if="editMode">
-        <template v-for="(contract, i) in contracts">
-          <v-col :key="contract.uuid" cols="12" sm="6" md="4">
-            <ContractListCard
-              :key="contract.uuid"
-              :data-cy="'contract-' + i"
-              :contract="contract"
-              :edit-mode="editMode"
-              @delete="confirmDelete(contract.uuid)"
-            />
-          </v-col>
-        </template>
-      </template>
+            <v-toolbar-title>
+              {{ editMode ? "Contracts" : "Select a contract" }}
+            </v-toolbar-title>
+          </v-toolbar>
+        </portal>
 
-      <template v-else>
-        <template v-for="(contract, i) in contracts">
-          <v-col :key="contract.uuid" cols="12" sm="6" md="4">
-            <ContractListCardSelect
-              :data-cy="'contract-' + i"
-              :contract="contract"
-              :edit-mode="editMode"
-              :disabled="clockedIntoContract(contract.uuid)"
-            />
-          </v-col>
-        </template>
-      </template>
-    </v-row>
+        <v-card-text>
+          <v-row :justify="loading ? 'center' : 'start'">
+            <v-col v-if="loading" cols="6" md="6">
+              <v-skeleton-loader
+                v-if="loading"
+                data-cy="skeleton"
+                type="card"
+                width="90%"
+                :loading="true"
+              ></v-skeleton-loader>
+            </v-col>
+
+            <template v-if="!loading && editMode">
+              <template v-for="(contract, i) in contracts">
+                <v-col :key="contract.uuid" cols="12" md="6">
+                  <ContractListCard
+                    :key="contract.uuid"
+                    :data-cy="'contract-' + i"
+                    :contract="contract"
+                    :edit-mode="editMode"
+                    @delete="confirmDelete(contract.uuid)"
+                  />
+                </v-col>
+              </template>
+            </template>
+
+            <template v-if="!loading && !editMode">
+              <template v-for="(contract, i) in contracts">
+                <v-col :key="contract.uuid" cols="12" md="6">
+                  <ContractListCardSelect
+                    :data-cy="'contract-' + i"
+                    :contract="contract"
+                    :edit-mode="editMode"
+                    :disabled="clockedIntoContract(contract.uuid)"
+                  />
+                </v-col>
+              </template>
+            </template>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-col>
 
     <TheDialog v-if="dialog" @close="dialog = false">
       <template v-slot:content>
@@ -75,28 +88,15 @@
       </template>
     </TheDialog>
 
-    <portal to="fab">
-      <v-btn
-        absolute
-        dark
-        fab
-        :top="$vuetify.breakpoint.mdAndUp"
-        :bottom="$vuetify.breakpoint.smAndDown"
-        right
-        color="secondary"
-        data-cy="contraxt-create-button"
-        :to="{ name: 'createContract' }"
-      >
-        <v-icon>{{ icons.mdiPlus }}</v-icon>
-      </v-btn>
-    </portal>
-  </v-container>
+    <TheFAB :to="{ name: 'createContract' }" />
+  </v-row>
 </template>
 
 <script>
 import ContractListCard from "@/components/contracts/ContractListCard";
 import ContractListCardSelect from "@/components/contracts/ContractListCardSelect";
 import TheDialog from "@/components/TheDialog";
+import TheFAB from "@/components/TheFAB";
 
 import ContractService from "@/services/contract";
 
@@ -110,7 +110,8 @@ export default {
   components: {
     ContractListCard,
     ContractListCardSelect,
-    TheDialog
+    TheDialog,
+    TheFAB
   },
   data() {
     return {
