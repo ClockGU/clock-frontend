1c0722c90295ca879b610309ea3dac275f2fe18a
Michael Gecht <gecht.m@gmail.com>
2020-01-29 08:58:31 +0100

    style(Dashboard): update style of Dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,14 +1,35 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col>
-        <v-card :elevation="$vuetify.breakpoint.smAndDown ? 0 : null">
-          <v-card-title>Dashboard</v-card-title>
-          <v-card-text>Some text</v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-row justify="center">
+    <v-col cols="12" md="6">
+      <v-card>
+        <!-- <portal-target name="card-toolbar"></portal-target> -->
+
+        <portal :to="$vuetify.breakpoint.smAndDown ? 'app-bar' : ''">
+          <v-toolbar slot-scope="{ action }" :elevation="0">
+            <v-app-bar-nav-icon
+              v-if="$vuetify.breakpoint.smAndDown"
+              icon
+              @click="action"
+            ></v-app-bar-nav-icon>
+
+            <v-toolbar-title>
+              Dashboard
+            </v-toolbar-title>
+          </v-toolbar>
+        </portal>
+
+        <v-card-text>Some dashboard information</v-card-text>
+      </v-card>
+    </v-col>
+
+    <v-col cols="12" md="6">
+      <v-card>
+        <v-card-title>Stats</v-card-title>
+
+        <v-card-text>Some dashboard information</v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
