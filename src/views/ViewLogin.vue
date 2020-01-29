abdb6f327cc0d3da4f0b5b2d8c5e05677d052b32
Michael Gecht <gecht.m@gmail.com>
2020-01-29 09:55:22 +0100

    style(ViewLogin): update style

--- src/views/ViewLogin.vue
+++ src/views/ViewLogin.vue
@@ -1,6 +1,9 @@
 <template>
-  <v-row :align="$vuetify.breakpoint.mdAndUp ? 'center' : ''" justify="center">
-    <v-col cols="12" sm="8" md="7" class="py-0">
+  <v-row
+    :align="$vuetify.breakpoint.mdAndUp ? 'center' : ''"
+    :justify="$vuetify.breakpoint.mdAndUp ? 'center' : ''"
+  >
+    <v-col cols="12" md="6" class="py-0">
       <LoginForm />
     </v-col>
   </v-row>
