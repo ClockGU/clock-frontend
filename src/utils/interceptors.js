6b4b50447674c722357a990a5843d131e9a573af
Michael Gecht <gecht.m@gmail.com>
2020-02-02 15:25:42 +0100

    fix(interceptor): ignore 404 status in refresh catch

--- src/utils/interceptors.js
+++ src/utils/interceptors.js
@@ -99,12 +99,16 @@ export const handleTokenRefresh = async function(error, requestFn) {
         headers: {
           "Content-Type": "application/json;charset=UTF-8"
         }
-      }).catch(error => {
-        log("requestFn rejected:", error);
-        return Promise.reject(error);
       });
     })
     .catch(error => {
+      if (error.response.status !== 401) {
+        log("ignoring non-401 error");
+
+        // Return empty dummy data for our API response handlers
+        return Promise.reject(error);
+      }
+
       log("handleTokenRefresh rejected");
 
       // Refresh has failed - reject the original request
