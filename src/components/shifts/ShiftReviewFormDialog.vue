20d26ac7cb5e7705e64f38330c8ab2e8752385f5
Michael Gecht <gecht.m@gmail.com>
2020-02-05 21:08:05 +0100

    feat(ShiftReviewFormDialog): new component

---
+++ src/components/shifts/ShiftReviewFormDialog.vue
@@ -0,0 +1,110 @@
+<template>
+  <v-dialog
+    v-model="mainDialog"
+    :fullscreen="$vuetify.breakpoint.smAndDown"
+    max-width="600"
+    @click:outside="closeMainDialog"
+  >
+    <v-card>
+      <v-toolbar flat>
+        <v-btn
+          v-if="$vuetify.breakpoint.smAndDown"
+          icon
+          @click="closeMainDialog"
+        >
+          <v-icon>{{ icons.mdiClose }}</v-icon>
+        </v-btn>
+        <v-toolbar-title>
+          {{ uuid !== null ? "Update shift" : "Create shift" }}
+        </v-toolbar-title>
+        <v-spacer v-if="$vuetify.breakpoint.smAndDown"></v-spacer>
+        <v-toolbar-items v-if="$vuetify.breakpoint.smAndDown">
+          <v-btn v-if="uuid !== null" icon @click="confirmDialog = true">
+            <v-icon>{{ icons.mdiDelete }}</v-icon>
+          </v-btn>
+          <v-btn text @click="save">
+            Save
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+
+      <v-card-text class="pb-0">
+        <ShiftForm
+          :entity="shiftEntity"
+          :uuid="uuid"
+          :now="now"
+          @cancel="closeMainDialog"
+          @save="save"
+          @update="updateData"
+        />
+      </v-card-text>
+
+      <v-card-actions v-if="$vuetify.breakpoint.mdAndUp">
+        <v-btn
+          data-cy="shift-save"
+          text
+          :disabled="!formValid"
+          color="primary"
+          @click="save"
+        >
+          Save
+        </v-btn>
+        <v-btn data-cy="shift-cancel" text @click="closeMainDialog">
+          Cancel
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import ShiftForm from "@/components/shifts/ShiftForm";
+
+import { mdiDelete, mdiClose } from "@mdi/js";
+
+export default {
+  name: "ShiftReviewFormDialog",
+  components: { ShiftForm },
+  props: {
+    now: {
+      type: Date,
+      default: () => new Date()
+    },
+    shiftEntity: {
+      type: Object || null,
+      default: null
+    }
+  },
+  data: () => ({
+    confirmDialog: false,
+    formValid: true,
+    mainDialog: true,
+    icons: {
+      mdiDelete,
+      mdiClose
+    },
+    shiftToSave: null
+  }),
+  computed: {
+    uuid() {
+      return this.shiftEntity === null ? null : this.shiftEntity.uuid;
+    }
+  },
+  created() {
+    // Make a copy of the shift we will save.
+    this.shiftToSave = this.shiftEntity;
+  },
+  methods: {
+    updateData(event) {
+      this.shiftToSave = event.shift;
+      this.formValid = event.valid;
+    },
+    closeMainDialog() {
+      this.$emit("close");
+    },
+    save() {
+      this.$emit("update", this.shiftToSave);
+    }
+  }
+};
+</script>
