9ea8b911e175de1025c1586bec6b09b8409c8a8e
Michael Gecht <gecht.m@gmail.com>
2020-02-05 21:08:34 +0100

    refactor(ClockInOutCard): remove unnecessary code

--- src/components/ClockInOutCard.vue
+++ src/components/ClockInOutCard.vue
@@ -15,42 +15,18 @@
         <v-window-item :key="0">
           <ClockInOutCardClock
             :actions="{ data, duration, status, start, pause, destroy, save }"
+            @updateWindow="window += $event"
+          />
+        </v-window-item>
+
+        <v-window-item :key="1">
+          <ClockInOutCardForm
+            :clocked-shift="clockedShift"
+            :destroy="destroy"
+            @updateWindow="window += $event"
           />
         </v-window-item>
       </v-window>
-      <!-- <portal v-if="needsReview" to="dialog">
-          <ReviewShifts
-            :shift="shiftToReview"
-            :callbacks="callbacks"
-            @close="closeReview()"
-          />
-        </portal>
-
-        <portal v-if="reselectContract !== null" to="dialog">
-          <TheDialog>
-            <template v-slot:content>
-              <v-card data-cy="change-contract">
-                <v-card-title class="headline word-break">
-                  You are clocked into a different contract.
-                </v-card-title>
-                <v-card-text>
-                  You will be redirected to the contract selection screen.
-                </v-card-text>
-                <v-card-actions>
-                  <v-btn
-                    data-cy="redirect"
-                    color="primary"
-                    text
-                    @click="reselectContract"
-                  >
-                    OK
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </template>
-          </TheDialog>
-        </portal>
-      </div> -->
     </template>
   </ClockInOut>
 </template>
@@ -58,21 +34,14 @@
 <script>
 import ClockInOut from "@/components/ClockInOut";
 import ClockInOutCardClock from "@/components/ClockInOutCardClock";
-// import ReviewShifts from "@/components/ReviewShifts";
-// import TheDialog from "@/components/TheDialog";
-
-import { parseISO } from "date-fns";
-
-// import { mdiDelete, mdiInformation } from "@mdi/js";
+import ClockInOutCardForm from "@/components/ClockInOutCardForm";
 
 export default {
   name: "ClockInOutCard",
   components: {
     ClockInOut,
-    ClockInOutCardClock
-    // ClockInOut,
-    // ReviewShifts,
-    // TheDialog
+    ClockInOutCardClock,
+    ClockInOutCardForm
   },
   props: {
     selectedContract: {
@@ -86,58 +55,8 @@ export default {
   },
   data() {
     return {
-      window: 0,
-      icons: {},
-      clockInTimeout: false,
-      startDate: null,
-      contractDates: null,
-      needsReview: false,
-      callbacks: null,
-      shiftToReview: null
+      window: 0
     };
-  },
-  computed: {
-    contract() {
-      return this.$store.state.selectedContract;
-    },
-    disabled() {
-      // Always return false to make the button never disabled.
-      // We should catch all errors now, and disabling is not a priority right now.
-      return false;
-      // Return false if todays date does not overlap with the contract.
-      // return !areIntervalsOverlapping(
-      //   { start: this.contractDates.start, end: this.contractDates.end },
-      //   { start: new Date(), end: new Date() }
-      // );
-    }
-  },
-  watch: {
-    selectedContract(newValue) {
-      this.setContractDates(newValue);
-    }
-  },
-  methods: {
-    short(data, callbacks) {
-      this.shiftToReview = { start: data.startDate, contract: this.contract };
-      this.needsReview = true;
-      this.callbacks = callbacks;
-    },
-    overflow(data, callbacks) {
-      this.shiftToReview = { start: data.startDate, contract: this.contract };
-      this.needsReview = true;
-      this.callbacks = callbacks;
-    },
-    setContractDates(contract) {
-      if (contract === null) return;
-
-      this.contractDates = {
-        start: parseISO(contract.date.start),
-        end: parseISO(contract.date.end)
-      };
-    },
-    closeReview() {
-      this.needsReview = false;
-    }
   }
 };
 </script>
