d41ff464afdb409a1373770dac76f417494366d4
Michael Gecht <gecht.m@gmail.com>
2020-02-05 21:07:57 +0100

    feat(ClockInOutCardForm): new component

---
+++ src/components/ClockInOutCardForm.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-card>
+    <v-toolbar :elevation="0">
+      <v-btn icon @click="$emit('updateWindow', -1)">
+        <v-icon>{{ icons.mdiArrowLeft }}</v-icon>
+      </v-btn>
+      <v-toolbar-title>
+        Your clocked shift spans multiple days
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <v-card-text>
+      <ClockedShiftSplitWarning
+        v-if="clockedShift !== null"
+        :clocked-shift="clockedShift"
+        @save="save"
+      />
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import ClockedShiftSplitWarning from "@/components/ClockedShiftSplitWarning";
+
+import { mdiArrowLeft } from "@mdi/js";
+
+export default {
+  name: "ClockInOutCardForm",
+  components: { ClockedShiftSplitWarning },
+  props: {
+    clockedShift: {
+      type: Object,
+      required: true
+    },
+    destroy: {
+      type: Function,
+      required: true
+    }
+  },
+  data: () => ({
+    dialog: false,
+    icons: { mdiArrowLeft }
+  }),
+  methods: {
+    save() {
+      this.destroy(false).then(() => {
+        this.$store.dispatch("snackbar/setSnack", {
+          snack: "Saved shift(s).",
+          timeout: 4000,
+          color: "success"
+        });
+        this.$emit("updateWindow", -1);
+      });
+    }
+  }
+};
+</script>
