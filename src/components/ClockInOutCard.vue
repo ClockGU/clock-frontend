e0add0e86516bb29ed00df1b15c38686d8a51a82
Michael Gecht <gecht.m@gmail.com>
2020-02-05 18:40:19 +0100

    refactor(ClockInOutCard): create new component

--- src/components/ClockInOutCard.vue
+++ src/components/ClockInOutCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <ClockInOut :clocked-shift="clockedShift" @short="short" @overflow="overflow">
+  <ClockInOut :clocked-shift="clockedShift">
     <template
       v-slot="{
         data,
@@ -11,106 +11,14 @@
         save
       }"
     >
-      <v-card
-        :disabled="
-          (status === 'running' && duration === 0) || status === 'saving'
-        "
-        :loading="
-          (status === 'running' && duration === 0) || status === 'saving'
-        "
-      >
-        <v-toolbar :elevation="0">
-          <v-toolbar-title>
-            {{ duration === 0 ? "No running shift" : "Shift is running" }}
-          </v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-toolbar-items>
-            <v-scale-transition>
-              <v-btn
-                v-if="status === 'running' && duration !== 0"
-                icon
-                @click="destroy"
-              >
-                <v-icon>{{ icons.mdiDelete }}</v-icon>
-              </v-btn>
-            </v-scale-transition>
-          </v-toolbar-items>
-        </v-toolbar>
-
-        <v-card-text>
-          <v-row justify="center">
-            <span v-if="duration === 0" class="title font-weight-light">
-              Start a shift
-            </span>
-            <span v-else class="title font-weight-light">
-              {{ duration | toTime }}
-            </span>
-          </v-row>
-          <v-slide-x-transition>
-            <v-btn v-if="duration === 0" color="primary" block @click="start">
-              Clock in
-            </v-btn>
-            <v-btn
-              v-if="
-                (status === 'running' && duration !== 0) || status === 'saving'
-              "
-              color="red"
-              block
-              text
-              @click="save"
-            >
-              Clock out
-            </v-btn>
-          </v-slide-x-transition>
-          <!-- <v-btn @click="pause">Pause</v-btn> -->
-        </v-card-text>
-      </v-card>
-      <!-- <div>
-        <v-col cols="10">
-          <v-row>
-            <v-col xs="10">
-              <v-hover>
-                <v-btn
-                  slot-scope="{ hover }"
-                  data-cy="clock-in-out-button"
-                  outlined
-                  text
-                  width="150"
-                  :disabled="disabled"
-                  @click="action"
-                >
-                  <v-slide-y-transition hide-on-leave>
-                    <span v-if="status === 'idle'">
-                      Clock in
-                    </span>
-                  </v-slide-y-transition>
-                  <v-slide-y-transition hide-on-leave>
-                    <span
-                      v-if="
-                        (status === 'running' && (!hover || duration <= 5)) ||
-                          needsReview
-                      "
-                    >
-                      {{ duration | toTime }}
-                    </span>
-                  </v-slide-y-transition>
-                  <v-slide-y-transition hide-on-leave>
-                    <span
-                      v-if="
-                        (status === 'paused' || (hover && duration > 5)) &&
-                          !needsReview
-                      "
-                    >
-                      Clock out
-                    </span>
-                  </v-slide-y-transition>
-                </v-btn>
-              </v-hover>
-            </v-col>
-          </v-row>
-        </v-col>
-
-        <portal v-if="needsReview" to="dialog">
+      <v-window v-model="window">
+        <v-window-item :key="0">
+          <ClockInOutCardClock
+            :actions="{ data, duration, status, start, pause, destroy, save }"
+          />
+        </v-window-item>
+      </v-window>
+      <!-- <portal v-if="needsReview" to="dialog">
           <ReviewShifts
             :shift="shiftToReview"
             :callbacks="callbacks"
@@ -149,54 +57,26 @@
 
 <script>
 import ClockInOut from "@/components/ClockInOut";
+import ClockInOutCardClock from "@/components/ClockInOutCardClock";
 // import ReviewShifts from "@/components/ReviewShifts";
 // import TheDialog from "@/components/TheDialog";
 
-import {
-  addSeconds,
-  // areIntervalsOverlapping,
-  format,
-  parseISO
-} from "date-fns";
+import { parseISO } from "date-fns";
 
-import { mdiDelete } from "@mdi/js";
+// import { mdiDelete, mdiInformation } from "@mdi/js";
 
 export default {
   name: "ClockInOutCard",
   components: {
-    ClockInOut
+    ClockInOut,
+    ClockInOutCardClock
     // ClockInOut,
     // ReviewShifts,
     // TheDialog
   },
-  filters: {
-    toTime(seconds) {
-      let string = "";
-
-      const date = addSeconds(new Date(1970, 1, 1), seconds);
-      const days = Math.floor(seconds / (60 * 60 * 24));
-      const hours = format(date, "HH");
-      const minutes = format(date, "mm");
-      const sec = format(date, "ss");
-
-      if (days > 0) {
-        string += `${days} days, `;
-      }
-      if (hours !== "00") {
-        string += `${hours} hrs `;
-      }
-      if (minutes !== "00") {
-        string += `${minutes} mins `;
-      }
-      if (sec !== "00") {
-        string += `${sec} secs`;
-      }
-      return string;
-    }
-  },
   props: {
     selectedContract: {
-      type: Object,
+      type: Object || null,
       required: true
     },
     clockedShift: {
@@ -206,7 +86,8 @@ export default {
   },
   data() {
     return {
-      icons: { mdiDelete },
+      window: 0,
+      icons: {},
       clockInTimeout: false,
       startDate: null,
       contractDates: null,
@@ -247,13 +128,12 @@ export default {
       this.callbacks = callbacks;
     },
     setContractDates(contract) {
+      if (contract === null) return;
+
       this.contractDates = {
         start: parseISO(contract.date.start),
         end: parseISO(contract.date.end)
       };
-    },
-    buttonClass(hover) {
-      return hover ? "primary darken-1" : "primary";
     },
     closeReview() {
       this.needsReview = false;
---
+++ src/components/ClockInOutCardClock.vue
@@ -0,0 +1,127 @@
+<template>
+  <v-card
+    v-bind="$attrs"
+    :disabled="actions.status === 'saving'"
+    :loading="actions.status === 'saving'"
+  >
+    <v-toolbar :elevation="0">
+      <v-toolbar-title>
+        {{
+          actions.status === "running" ? "Shift is running" : "No running shift"
+        }}
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-scale-transition>
+          <v-btn
+            v-if="actions.status === 'running' || actions.status === 'saving'"
+            icon
+            color="error"
+          >
+            <v-icon>{{ icons.mdiInformation }}</v-icon>
+          </v-btn>
+        </v-scale-transition>
+        <v-scale-transition>
+          <v-btn
+            v-if="actions.status === 'running' || actions.status === 'saving'"
+            icon
+            @click="actions.destroy"
+          >
+            <v-icon>{{ icons.mdiDelete }}</v-icon>
+          </v-btn>
+        </v-scale-transition>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <v-card-text>
+      <v-row justify="center">
+        <span
+          v-if="actions.status !== 'running' && actions.status !== 'saving'"
+          class="title font-weight-light"
+        >
+          Start a shift
+        </span>
+        <div v-else class="d-flex flex-column">
+          <div class="font-weight-light">
+            {{ actions.data.startDate }}
+          </div>
+          <div class="title font-weight-light text-center">
+            {{ actions.duration | toTime }}
+          </div>
+        </div>
+      </v-row>
+      <v-slide-x-transition group leave-absolute>
+        <v-btn
+          v-if="actions.duration === 0 && actions.status === 'idle'"
+          key="clock-in"
+          :disabled="actions.status === 'saving'"
+          color="primary"
+          block
+          @click="actions.start"
+        >
+          Clock in
+        </v-btn>
+        <v-btn
+          v-else
+          key="clock-out"
+          :disabled="actions.status === 'saving'"
+          color="red"
+          block
+          text
+          @click="actions.save"
+        >
+          Clock out
+        </v-btn>
+      </v-slide-x-transition>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import { addSeconds, format } from "date-fns";
+import { mdiDelete, mdiInformation } from "@mdi/js";
+
+export default {
+  name: "ClockInOutCardClock",
+  filters: {
+    toTime(seconds) {
+      let string = "";
+
+      const date = addSeconds(new Date(1970, 1, 1), seconds);
+      const days = Math.floor(seconds / (60 * 60 * 24));
+      const hours = format(date, "HH");
+      const minutes = format(date, "mm");
+      const sec = format(date, "ss");
+
+      if (days > 0) {
+        string += `${days} days, `;
+      }
+      if (hours !== "00") {
+        string += `${hours} hrs `;
+      }
+      if (minutes !== "00") {
+        string += `${minutes} mins `;
+      }
+      if (sec !== "00") {
+        string += `${sec} secs`;
+      }
+
+      if (string === "") return "00 secs";
+
+      return string;
+    }
+  },
+  inheritAttrs: false,
+  props: {
+    actions: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      icons: { mdiDelete, mdiInformation }
+    };
+  }
+};
+</script>
