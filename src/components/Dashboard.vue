f5e525bcda9543482b4710f080b62402d446cc90
Michael Gecht <gecht.m@gmail.com>
2020-02-05 21:53:53 +0100

    refactor(Dashboard): remove commented html

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,8 +2,6 @@
   <v-row justify="center">
     <v-col cols="12" md="6">
       <v-card>
-        <!-- <portal-target name="card-toolbar"></portal-target> -->
-
         <portal :to="$vuetify.breakpoint.smAndDown ? 'app-bar' : ''">
           <v-toolbar slot-scope="{ action }" :elevation="0">
             <v-app-bar-nav-icon
