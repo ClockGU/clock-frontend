7465db82c61e741b91c5185074e95b1bf4d733bd
Michael Gecht <gecht.m@gmail.com>
2020-04-13 10:52:46 +0200

    refactor(Dashboard): use base-layout

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,32 +1,19 @@
 <template>
-  <v-row justify="center">
-    <v-col cols="12" md="6">
-      <v-card>
-        <portal :to="$vuetify.breakpoint.smAndDown ? 'app-bar' : ''">
-          <v-toolbar slot-scope="{ action }" :elevation="0">
-            <v-app-bar-nav-icon
-              v-if="$vuetify.breakpoint.smAndDown"
-              icon
-              @click="action"
-            ></v-app-bar-nav-icon>
+  <base-layout>
+    <template v-slot:title>Dashboard</template>
+    <template v-slot:text>
+      <v-card-text>Some dashboard information</v-card-text>
+    </template>
 
-            <v-toolbar-title>
-              Dashboard
-            </v-toolbar-title>
-          </v-toolbar>
-        </portal>
-
-        <v-card-text>Some dashboard information</v-card-text>
-      </v-card>
-    </v-col>
-
-    <v-col cols="12" md="6">
-      <ClockInOutCard
-        :clocked-shift="clockedShift"
-        :selected-contract="selectedContract"
-      />
-    </v-col>
-  </v-row>
+    <template v-slot:extra-content>
+      <v-col cols="12" md="6">
+        <ClockInOutCard
+          :clocked-shift="clockedShift"
+          :selected-contract="selectedContract"
+        />
+      </v-col>
+    </template>
+  </base-layout>
 </template>
 
 <script>
