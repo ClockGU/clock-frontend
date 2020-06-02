633b9bbfbb470453424a7c1c3a50204d63c8d73b
Michael Gecht <gecht.m@gmail.com>
2020-06-02 19:41:11 +0200

    project: make development environment available via URL

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,11 @@ const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 module.exports = {
   transpileDependencies: ["vuetify"],
   productionSourceMap: true,
+  devServer: {
+    public: process.env.VUE_APP_PUBLIC_URL,
+    allowedHosts: [process.env.VUE_APP_ALLOWED_HOST],
+    disableHostCheck: true
+  },
   configureWebpack: config => {
     if (
       process.env.NODE_ENV === "production" ||
