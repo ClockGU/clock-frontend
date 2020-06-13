7e083f3d96f8d9e689585c0900b90b5ba5d4cf6e
Michael Gecht <gecht.m@gmail.com>
2020-06-13 18:27:57 +0200

    feat(LanguageSettings): new component

---
+++ src/components/LanguageSettings.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card :elevation="$vuetify.breakpoint.mdAndUp ? 0 : null">
+    <v-card-text :class="$vuetify.breakpoint.mdAndUp ? 'py-0' : ''">
+      <LanguageSwitcher />
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import LanguageSwitcher from "@/components/LanguageSwitcher";
+
+export default {
+  name: "LanguageSettings",
+  components: { LanguageSwitcher }
+};
+</script>
