45d471c668e5590ab387e6de85bc94eb2910851e
Michael Gecht <gecht.m@gmail.com>
2020-06-20 10:11:19 +0200

    squash reports

--- src/services/report.js
+++ src/services/report.js
@@ -1,5 +1,33 @@
 import ApiService from "@/services/api";
 
+function timedeltaToMinutes(timedelta) {
+  const splitTimedelta = timedelta.split(" ");
+  let days = 0;
+  let timeString = splitTimedelta[0];
+
+  if (splitTimedelta.length == 2) {
+    [days, timeString] = splitTimedelta;
+  }
+  const [hours, minutes, seconds] = timeString.split(":");
+
+  return (
+    (parseInt(days) * 24 + parseInt(hours)) * 60 +
+    parseInt(minutes) +
+    parseInt(seconds) / 60
+  );
+}
+
+function mapApiResponse(response) {
+  return {
+    uuid: response.id,
+    contract: response.contract,
+    duration: timedeltaToMinutes(response.hours),
+    hours: response.hours,
+    date: response.month_year,
+    exported: false
+  };
+}
+
 const BASE_URL = "/reports/";
 
 const ReportService = {
@@ -10,6 +38,19 @@ const ReportService = {
       responseType: "arraybuffer"
     };
     return ApiService.customRequest(requestData);
+  },
+  list: async function() {
+    return new Promise((resolve, reject) => {
+      ApiService.get(BASE_URL)
+        .then(response => {
+          const data = response.data.map(item => mapApiResponse(item));
+          const newResponse = { ...response, data };
+          resolve(newResponse);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
   }
 };
 
