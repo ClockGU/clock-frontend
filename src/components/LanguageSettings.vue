87b9dce76645aefb37caf74a3ff3c798fd61d02b
Michael Gecht <gecht.m@gmail.com>
2020-06-25 07:55:23 +0200

    feat(LanguageSwitcher): add text

--- src/components/LanguageSettings.vue
+++ src/components/LanguageSettings.vue
@@ -1,8 +1,12 @@
 <template>
   <v-card :elevation="$vuetify.breakpoint.mdAndUp ? 0 : null">
     <v-card-text :class="$vuetify.breakpoint.mdAndUp ? 'py-0' : ''">
-      <LanguageSwitcher @updateLocale="update" />
+      {{ $t("settings.language.text") }}
     </v-card-text>
+
+    <v-card-actions>
+      <LanguageSwitcher @updateLocale="update" />
+    </v-card-actions>
   </v-card>
 </template>
 
