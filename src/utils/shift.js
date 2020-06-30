55aac0ebb922786e6993ad499956489276a430f2
Michael Gecht <gecht.m@gmail.com>
2020-06-30 20:52:35 +0200

    feat(utils/shift): add getOverlappingShifts fn

--- src/utils/shift.js
+++ src/utils/shift.js
@@ -1,5 +1,12 @@
 import { Shift } from "@/models/ShiftModel";
-import { format, parseISO } from "date-fns";
+import { areIntervalsOverlapping, format, parseISO } from "date-fns";
+
+function prepare({ start, end }) {
+  return {
+    start: parseISO(start),
+    end: parseISO(end)
+  };
+}
 
 export function datesGroupByComponent(dates, token) {
   return dates.reduce(function(val, obj) {
@@ -7,4 +14,21 @@ export function datesGroupByComponent(dates, token) {
     (val[comp] = val[comp] || []).push(new Shift(obj));
     return val;
   }, {});
+}
+
+export function getOverlappingShifts(shifts) {
+  const matches = [];
+  for (let i = 0; i <= shifts.length - 1; i++) {
+    for (let j = i + 1; j <= shifts.length - 1; j++) {
+      if (j == shifts.length) break;
+
+      const shiftA = shifts[i];
+      const shiftB = shifts[j];
+
+      if (areIntervalsOverlapping(prepare(shiftA.date), prepare(shiftB.date))) {
+        matches.push([shiftA, shiftB]);
+      }
+    }
+  }
+  return matches;
 }
