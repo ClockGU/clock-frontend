75dd06441944d154fb1713c5d9fd8bd5f576877c
Michael Gecht <gecht.m@gmail.com>
2020-07-07 19:21:49 +0200

    refactor(DashboardLastAcitivty): create component
    - Sort shifts by newest first

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -43,32 +43,7 @@
       </v-col>
 
       <v-col cols="12" md="6">
-        <v-card>
-          <v-card-title>
-            {{ $t("dashboard.lastActivity") }}
-          </v-card-title>
-          <v-card-text>
-            <template v-if="lastFiveShifts.length > 0">
-              <v-list>
-                <v-list-item-group>
-                  <template v-for="shift in lastFiveShifts">
-                    <ShiftListItem
-                      :key="shift.uuid"
-                      :editable="false"
-                      :item="shift"
-                    />
-                  </template>
-                </v-list-item-group>
-              </v-list>
-              <v-btn color="success" text :to="allShiftRouter">
-                {{ $t("dashboard.showAll") }}
-              </v-btn>
-            </template>
-            <v-container v-else>
-              {{ $t("dashboard.emptyActivity") }}
-            </v-container>
-          </v-card-text>
-        </v-card>
+        <DashboardLastActivity />
       </v-col>
     </v-row>
   </v-container>
@@ -78,15 +53,13 @@
 import ClockInOutCard from "@/components/ClockInOutCard";
 import DashboardShiftButton from "@/components/DashboardShiftButton";
 import MonthlyProgress from "@/components/MonthlyProgress";
-import ShiftListItem from "@/components/shifts/ShiftListItem";
 import SelectContractFilter from "@/components/SelectContractFilter";
 import DashboardConflicts from "@/components/DashboardConflicts";
+import DashboardLastActivity from "@/components/DashboardLastActivity";
 
 import { Contract } from "@/models/ContractModel";
 
 import { mapGetters } from "vuex";
-
-import { Shift } from "@/models/ShiftModel";
 
 import contractExpiredMixin from "@/mixins/contractExpired";
 
@@ -102,16 +75,16 @@ export default {
     ClockInOutCard,
     DashboardShiftButton,
     MonthlyProgress,
-    ShiftListItem,
+
     SelectContractFilter,
-    DashboardConflicts
+    DashboardConflicts,
+    DashboardLastActivity
   },
   mixins: [contractExpiredMixin],
   data: () => ({
     length: 3,
     step: 0,
     entity: new Contract(),
-    allShiftRouter: { name: "shiftList" },
     loading: true
   }),
   computed: {
@@ -125,12 +98,6 @@ export default {
       const uuid = this.$route.params.contract;
 
       return this.contracts.find(contract => contract.uuid === uuid);
-    },
-    lastFiveShifts() {
-      return this.shifts
-        .map(shift => new Shift(shift))
-        .filter(shift => shift.contract === this.selectedContract.uuid)
-        .slice(0, 5);
     },
     latestReport() {
       const reports = this.reports
