1ae5a2217a9e640d181cb608f91f462426f02d56
Bertram Bühner <github@sheepyhollow.de>
2020-07-09 00:52:59 +0200

    hero locale strings

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -14,12 +14,12 @@
           width="100%"
         >
           <h1 class="text-1 font-weight-black mb-4">
+            {{ $t("hero.slogan") }}
             Bringe deine Stundenzettel unter Kontrolle
           </h1>
 
           <p>
-            Mit Clock kannst du deinen Stundenzettel ganz bequem unterwegs oder
-            am Computer bearbeiten.
+            {{ $t("hero.text") }}
           </p>
 
           <div
@@ -31,7 +31,7 @@
             class="d-flex flex-wrap"
           >
             <ButtonGoetheOAuth>
-              Login mit HRZ Account
+              {{ $t("hero.login") }}
             </ButtonGoetheOAuth>
           </div>
         </v-responsive>
