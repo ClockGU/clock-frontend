d78b12c157b873016a11f7a4155efe859258a57b
Bertram Bühner <github@sheepyhollow.de>
2021-01-04 21:23:38 +0100

    Update dashboard welcome message

--- src/components/DashboardWelcome.vue
+++ src/components/DashboardWelcome.vue
@@ -1,9 +1,7 @@
 <template>
   <v-card outlined>
     <v-card-text class="body-1">
-      Willkommen bei Clock! Wir sind im Hintergrund fleißig mit der
-      Weiterentwicklung beschäftigt und Du darfst regelmäßige Updates erwarten.
-      Wenn Du mal nicht weiter weißt, wirf einen Blick ins
+      Willkommen bei Clock! Wir arbeiten stetig daran, die Anwendung zu verbessern. Wenn Du mal nicht weiter weißt, wirf einen Blick ins
       <base-link href="https://clockgu.gitbook.io/clock-benutzerhandbuch/"
         >Handbuch</base-link
       >
