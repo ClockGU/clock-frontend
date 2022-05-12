a738dadf8d74b9c068f537efc9865dd25e526464
Christian Grossmüller <chgad.games@gmail.com>
2022-05-12 18:15:35 +0200

    Edit router guard WIP.

--- src/router/guards.js
+++ src/router/guards.js
@@ -6,27 +6,31 @@ export async function RequiredDataGuard(to, from, next) {
   if (to.query.poll !== undefined && to.query.poll === false) {
     return next();
   }
-
   try {
-    // eslint-disable-next-line no-unused-vars
-    const [shifts, contracts, reports] = await Promise.all([
-      store.dispatch("shift/queryShifts"),
-      store.dispatch("contract/queryContracts"),
-      store.dispatch("report/list")
-    ]);
-
+    const contracts = await store.dispatch("contract/queryContracts");
     const contractMatch = contracts.find(
       (contract) => contract.uuid === to.params.contract
     );
-    // Redirect to 404 page we are looking for a contract that does not exist
-    if (to.params.contract !== undefined && !contractMatch) {
-      return next({ name: "404" });
+    if (to.params.contract !== undefined) {
+      // Redirect to 404 page we are looking for a contract that does not exist
+      if (!contractMatch) {
+        return next({ name: "404" });
+      }
+      // go on to valid, matched contract.
+      // TODO: This call is mentioned twice.
+      await store.dispatch("report/list");
+      return next();
     }
 
     // If we match a route that requires a contract to be set
-    // but the user did not set one, we get the contract with
+    // but the user did not set one, yet he has existing contracts, we get the contract with
     // the latest activity
-    if (to.params.contract === undefined) {
+    if (contracts.length > 0) {
+      // eslint-disable-next-line no-unused-vars
+      const [shifts, reports] = await Promise.all([
+        store.dispatch("shift/queryShifts"),
+        store.dispatch("reports/list")
+      ]);
       const uuid = getContractWithLastActivity({ shifts, contracts });
       return next(getNextContractParams(to, uuid));
     }
