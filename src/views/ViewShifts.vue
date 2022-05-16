261cc5a16f4701a8d56bf3e877af1ff057c99593
Christian Grossmüller <chgad.games@gmail.com>
2022-05-16 16:40:28 +0200

    Make ShiftTableView work without contract.

--- src/views/ViewShifts.vue
+++ src/views/ViewShifts.vue
@@ -3,11 +3,11 @@
     <v-row>
       <v-col cols="12">
         <SelectContractFilter
+          :disabled="disabled"
           :contracts="contracts"
           :selected-contract="selectedContract"
         />
       </v-col>
-
       <v-col>
         <DataFilter
           :date="date"
@@ -17,124 +17,149 @@
         >
           <template #default="{ data }">
             <v-card>
-              <v-toolbar flat>
-                <v-row>
-                  <v-col>
-                    <v-btn color="primary" @click="newShift">
-                      {{
-                        $t("buttons.newEntity", { entity: $tc("models.shift") })
-                      }}
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-toolbar>
+              <v-hover>
+                <template #default="{ hover }">
+                  <div @click="toggleTouchOverlay(hover)">
+                    <v-toolbar flat>
+                      <v-row>
+                        <v-col>
+                          <v-btn
+                            :disabled="disabled"
+                            color="primary"
+                            @click="newShift"
+                          >
+                            {{
+                              $t("buttons.newEntity", {
+                                entity: $tc("models.shift")
+                              })
+                            }}
+                          </v-btn>
+                        </v-col>
+                      </v-row>
+                    </v-toolbar>
 
-              <MonthSwitcher :data="data" :date="date" @update="updateDate" />
+                    <MonthSwitcher
+                      :data="data"
+                      :date="date"
+                      @update="updateDate"
+                    />
 
-              <ShiftsTable
-                :shifts="data.pastShifts"
-                :loading="loading"
-                :search="pastSearch"
-                past-shifts
-                @edit="editShift"
-                @refresh="refresh"
-              >
-                <template #head="{ destroyFn, selected }">
-                  <v-card-title>
-                    <v-row>
-                      <v-col cols="12" md="5">
-                        <span>
-                          {{ $t("shifts.table.pastShiftsTitle") }}
-                          {{ durationSum(data.pastShifts) }}
-                        </span>
-                      </v-col>
+                    <ShiftsTable
+                      :shifts="data.pastShifts"
+                      :loading="loading"
+                      :search="pastSearch"
+                      past-shifts
+                      @edit="editShift"
+                      @refresh="refresh"
+                    >
+                      <template #head="{ destroyFn, selected }">
+                        <v-card-title>
+                          <v-row>
+                            <v-col cols="12" md="5">
+                              <span>{{
+                                $t("shifts.table.pastShiftsTitle")
+                              }}</span>
+                            </v-col>
 
-                      <v-spacer></v-spacer>
+                            <v-spacer></v-spacer>
 
-                      <v-col
-                        cols="12"
-                        sm="5"
-                        :offset-sm="selected.length > 0 ? 0 : 7"
-                        offset-md="0"
-                        md="3"
+                            <v-col
+                              cols="12"
+                              sm="5"
+                              :offset-sm="selected.length > 0 ? 0 : 7"
+                              offset-md="0"
+                              md="3"
+                            >
+                              <v-text-field
+                                v-model="pastSearch"
+                                :append-icon="icons.mdiMagnify"
+                                :label="$t('actions.search')"
+                                dense
+                                hide-details
+                              ></v-text-field>
+                            </v-col>
+                          </v-row>
+                        </v-card-title>
+
+                        <v-card-text>
+                          {{ $t("shifts.table.pastShiftsHint") }}
+                        </v-card-text>
+
+                        <ShiftBulkActions
+                          v-if="selected.length > 0"
+                          :destroy-fn="destroyFn"
+                          :shifts="selected"
+                          can-review
+                          @edit="editShift"
+                          @refresh="refresh"
+                        />
+                      </template>
+                    </ShiftsTable>
+
+                    <ShiftsTable
+                      :shifts="data.futureShifts"
+                      :loading="loading"
+                      :search="futureSearch"
+                      @edit="editShift"
+                      @refresh="refresh"
+                    >
+                      <template #head="{ destroyFn, selected }">
+                        <v-card-title>
+                          <v-row>
+                            <v-col cols="12" md="5">
+                              <span>{{
+                                $t("shifts.table.futureShiftsTitle")
+                              }}</span>
+                            </v-col>
+
+                            <v-spacer></v-spacer>
+
+                            <v-col
+                              cols="12"
+                              sm="5"
+                              :offset-sm="selected.length > 0 ? 0 : 7"
+                              offset-md="0"
+                              md="3"
+                            >
+                              <v-text-field
+                                v-model="futureSearch"
+                                :append-icon="icons.mdiMagnify"
+                                :label="$t('actions.search')"
+                                dense
+                                hide-details
+                              ></v-text-field>
+                            </v-col>
+                          </v-row>
+                        </v-card-title>
+
+                        <v-card-text>
+                          {{ $t("shifts.table.futureShiftsHint") }}
+                        </v-card-text>
+                        <ShiftBulkActions
+                          v-if="selected.length > 0"
+                          :shifts="selected"
+                          :destroy-fn="destroyFn"
+                          @edit="editShift"
+                          @refresh="refresh"
+                        />
+                      </template>
+                    </ShiftsTable>
+                    <v-fade-transition>
+                      <v-overlay
+                        v-if="disabled && (hover || touchOverlay)"
+                        absolute
+                        color="primary"
+                        style="align-items: start"
                       >
-                        <v-text-field
-                          v-model="pastSearch"
-                          :append-icon="icons.mdiMagnify"
-                          :label="$t('actions.search')"
-                          dense
-                          hide-details
-                        ></v-text-field>
-                      </v-col>
-                    </v-row>
-                  </v-card-title>
-
-                  <v-card-text>
-                    {{ $t("shifts.table.pastShiftsHint") }}
-                  </v-card-text>
-
-                  <ShiftBulkActions
-                    v-if="selected.length > 0"
-                    :destroy-fn="destroyFn"
-                    :shifts="selected"
-                    :contracts="contracts"
-                    can-review
-                    @edit="editShift"
-                    @refresh="refresh"
-                  />
+                        <p style="margin-top: 17%" class="text-center">
+                          Hier kannst du deine gespeicherten Schichten ansehen
+                          und verwalten.
+                        </p>
+                      </v-overlay>
+                    </v-fade-transition>
+                  </div>
                 </template>
-              </ShiftsTable>
-
-              <ShiftsTable
-                :shifts="data.futureShifts"
-                :loading="loading"
-                :search="futureSearch"
-                @edit="editShift"
-                @refresh="refresh"
-              >
-                <template #head="{ destroyFn, selected }">
-                  <v-card-title>
-                    <v-row>
-                      <v-col cols="12" md="5">
-                        <span>
-                          {{ $t("shifts.table.futureShiftsTitle") }}
-                          {{ durationSum(data.futureShifts) }}
-                        </span>
-                      </v-col>
-
-                      <v-spacer></v-spacer>
-
-                      <v-col
-                        cols="12"
-                        sm="5"
-                        :offset-sm="selected.length > 0 ? 0 : 7"
-                        offset-md="0"
-                        md="3"
-                      >
-                        <v-text-field
-                          v-model="futureSearch"
-                          :append-icon="icons.mdiMagnify"
-                          :label="$t('actions.search')"
-                          dense
-                          hide-details
-                        ></v-text-field>
-                      </v-col>
-                    </v-row>
-                  </v-card-title>
-
-                  <v-card-text>
-                    {{ $t("shifts.table.futureShiftsHint") }}
-                  </v-card-text>
-                  <ShiftBulkActions
-                    v-if="selected.length > 0"
-                    :shifts="selected"
-                    :contracts="contracts"
-                    :destroy-fn="destroyFn"
-                    @edit="editShift"
-                    @refresh="refresh"
-                  />
-                </template>
-              </ShiftsTable>
+              </v-hover>
             </v-card>
           </template>
         </DataFilter>
@@ -183,19 +208,28 @@ export default {
     shiftEntity: null,
     showFormDialog: false,
     pastSearch: "",
-    futureSearch: ""
+    futureSearch: "",
+    touchOverlay: false
   }),
   computed: {
     ...mapGetters({
       contracts: "contract/contracts"
     }),
+    disabled() {
+      return this.$route.params.contract === undefined;
+    },
     selectedContract() {
       const uuid = this.$route.params.contract;
-
+      if (this.disabled) {
+        return { uuid: null, date: { start: "2019-01-01", end: "2019-01-31" } };
+      }
       return this.contracts.find((contract) => contract.uuid === uuid);
     }
   },
   methods: {
+    toggleTouchOverlay(hover) {
+      this.touchOverlay = hover ? false : !this.touchOverlay;
+    },
     editShift(shift) {
       this.shiftEntity = shift;
       this.showFormDialog = true;
