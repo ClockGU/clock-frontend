a362af3b65593b40f992e18910bedb196c346846
Christian Grossmüller <chgad.games@gmail.com>
2022-05-27 12:14:03 +0200

    New structure.

---
+++ src/components/gdpr/text-components/GdprTextDialog.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-dialog v-model="dialog" scrollable max-width="600">
+    <template #activator="{ on, attrs }">
+      <a v-bind="attrs" v-on="on">{{ $t("app.privacyagreement") }}</a>
+    </template>
+    <GdprTextCard @close="dialog = false"></GdprTextCard>
+  </v-dialog>
+</template>
+
+<script>
+import GdprTextCard from "@/components/gdpr/text-components/GdprTextCard";
+export default {
+  name: "GdprTextDialog",
+  components: { GdprTextCard },
+  data() {
+    return {
+      dialog: false
+    };
+  }
+};
+</script>
