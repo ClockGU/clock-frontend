1e9736161f8c42221406bd8672fcb6d75c9d71f2
Christian Grossmüller <chgad.games@gmail.com>
2022-08-26 17:31:58 +0200

    First Design of ContractModel and restrucutreing of helper functions.

--- src/models/ContractModel.js
+++ src/models/ContractModel.js
@@ -1,128 +1,90 @@
 import is from "ramda/src/is";
 import { format, differenceInCalendarDays, startOfMonth } from "date-fns";
-import { defaultContractDate } from "@/utils/date";
 
-Number.prototype.pad = function (size) {
-  var s = String(this);
-  while (s.length < (size || 2)) {
-    s = "0" + s;
-  }
-  return s;
-};
+function mapContractApiResponse(response) {
+  return {
+    id: response.id,
+    user: response.user,
+    name: response.name,
+    minutes: response.minutes,
+    startDate: response.start_date,
+    endDate: response.end_date,
+    initialCarryoverMinutes: response.initial_carryover_minutes,
+    carryoverTargetDate: response.carryover_target_date,
+    createdAt: response.created_at,
+    modifiedAt: response.modified_at,
+    lastUsed: response.last_used
+  };
+}
+
 export class Contract {
   constructor({
-    uuid = null,
+    id = null,
     user = null,
     name = null,
-    worktime = null,
-    date = { start: null, end: null },
+    minutes = null,
+    startDate = null,
+    endDate = null,
+    initialCarryoverMinutes = null,
     carryoverTargetDate = null,
-    carryoverTime = null
+    createdAt = null,
+    modifiedAt = null,
+    lastUsed = null
   } = {}) {
-    this.uuid = is(String, uuid) ? uuid : null;
+    this.id = is(String, id) ? id : null;
     this.user = is(String, user) ? user : null;
     this.name = is(String, name) ? name : null;
-    this.worktime = is(Number, worktime)
-      ? this.minutesToTimestring(worktime)
-      : "";
-    this.date = {
-      start: is(Date, new Date(date.start))
-        ? date.start
-        : defaultContractDate({ type: "start" }),
-      end: is(Date, new Date(date.end))
-        ? date.end
-        : defaultContractDate({ type: "end" })
-    };
+    this.minutes = is(Number, minutes) ? minutes : 0;
+    this.startDate = is(Date, new Date(startDate)) ? new Date(startDate) : null;
+    this.endDate = is(Date, new Date(endDate)) ? new Date(endDate) : null;
+    this.initialCarryoverMinutes = is(Number, initialCarryoverMinutes)
+      ? initialCarryoverMinutes
+      : 0;
     this.carryoverTargetDate =
       is(Date, new Date(carryoverTargetDate)) && carryoverTargetDate !== null
         ? carryoverTargetDate
-        : startOfMonth(this.date.start);
-    this.carryoverTime = is(Number, carryoverTime)
-      ? this.minutesToTimestring(carryoverTime)
-      : "";
-  }
-
-  get start() {
-    return this.date.start;
-  }
-
-  set start(value) {
-    this.date.start = value;
-  }
-
-  get end() {
-    return this.date.end;
-  }
-
-  set end(value) {
-    this.date.end = value;
+        : startOfMonth(this.startDate);
+    this.createdAt = is(Date, new Date(createdAt)) ? new Date(createdAt) : null;
+    this.modifiedAt = is(Date, new Date(modifiedAt))
+      ? new Date(modifiedAt)
+      : null;
+    this.lastUsed = is(Date, new Date(lastUsed)) ? new Date(lastUsed) : null;
   }
 
   get duration() {
-    return differenceInCalendarDays(this.end, this.start);
+    return differenceInCalendarDays(this.endDate, this.startDate);
   }
 
   get remainingContractDuration() {
-    return differenceInCalendarDays(this.end, new Date());
+    return differenceInCalendarDays(this.endDate, new Date());
   }
 
-  get hoursInMinutes() {
-    const [hours, minutes] = this.worktime.split(":");
-
-    return hours * 60 + minutes;
+  get minutesToHourRepresentation() {
+    // Getter converting minutes into decimal representation of hours
+    const remainingMinutes = this.minutes % 60;
+    const fullHours = this.minutes - remainingMinutes;
+    return fullHours + remainingMinutes / 60;
   }
 
-  timestringToMinutes(time) {
-    if (time == "") {
-      return 0;
-    }
-
-    let negative = false;
-    let [hours, minutes] = time.split(":");
-
-    // Is the time negative?
-    if (hours.indexOf("-") === 0) {
-      hours = hours.slice(1);
-      negative = true;
-    }
-
-    minutes = parseFloat(hours) * 60 + parseFloat(minutes);
-
-    if (negative) {
-      minutes = minutes * -1;
-    }
-
-    return minutes;
+  CarryoverTargetDateString() {
+    return format(startOfMonth(this.carryoverTargetDate), "yyyy-MM-dd");
+  }
+  startDateString() {
+    return format(startOfMonth(this.startDate), "yyyy-MM-dd");
   }
 
-  minutesToTimestring(value) {
-    let sign = "";
-
-    if (value < 0) {
-      sign = "-";
-    }
-    const positiveValue = Math.abs(value);
-
-    const hours = Math.floor(positiveValue / 60);
-    const minutes = positiveValue % 60;
-
-    return `${sign}${hours.pad(2)}:${minutes.pad(2)}`;
-  }
-
-  convertCarryoverTargetDate() {
-    const targetDate = new Date(this.carryoverTargetDate);
-
-    return format(startOfMonth(targetDate), "yyyy-MM-dd");
+  endDateString() {
+    return format(startOfMonth(this.endDate), "yyyy-MM-dd");
   }
 
   toPayload() {
     return {
       name: this.name,
-      minutes: this.timestringToMinutes(this.worktime),
-      start_date: this.start,
-      end_date: this.end,
-      carryover_target_date: this.convertCarryoverTargetDate(),
-      initial_carryover_minutes: this.timestringToMinutes(this.carryoverTime)
+      minutes: this.minutes,
+      start_date: this.startDateString(),
+      end_date: this.endDateString(),
+      carryover_target_date: this.CarryoverTargetDateString(),
+      initial_carryover_minutes: this.initialCarryoverMinutes
     };
   }
 }
