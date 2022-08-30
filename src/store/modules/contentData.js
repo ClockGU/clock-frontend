9a4fe4ca49663700f8e14fb93afb5579b8bde125
Christian Grossmüller <chgad.games@gmail.com>
2022-08-30 12:10:20 +0200

    Add all<object-name> getters to contentData store.

--- src/store/modules/contentData.js
+++ src/store/modules/contentData.js
@@ -11,6 +11,27 @@ const getters = {
   selectedReports({ state, rootGetters }) {
     const selectedContractId = rootGetters["contract/selectedContract"].id;
     return state.contentData[selectedContractId].reports;
+  },
+  allShifts(state) {
+    let shiftArray = [];
+    for (let data of Object.values(state.contentData)) {
+      shiftArray.push(...data.shifts);
+    }
+    return shiftArray;
+  },
+  allReports(state) {
+    let reportArray = [];
+    for (let data of Object.values(state.contentData)) {
+      reportArray.push(...data.reports);
+    }
+    return reportArray;
+  },
+  allContracts(state) {
+    let contractArray = [];
+    for (let data of Object.values(state.contentData)) {
+      contractArray.push(data.contract);
+    }
+    return contractArray;
   }
 };
 
