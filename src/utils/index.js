b8e8dc3290629b53cf2362ce7edf497029d551f6
Christian Grossmüller <chgad.games@gmail.com>
2022-09-05 16:27:00 +0200

    Implement sort generating function and implement sorted data saving.

--- src/utils/index.js
+++ src/utils/index.js
@@ -30,18 +30,23 @@ export const indexOfByStarted = indexOfBy("started");
 export const indexOfByStartDate = indexOfBy("startDate");
 export const indexOfByMonthYear = indexOfBy("monthYear");
 
-export function sortObjects(objects, key) {
+const sortBy = (key) => (objects) => {
   return objects.sort((a, b) => b[key] - a[key]);
-}
+};
+
+export const sortByStarted = sortBy("started");
+export const sortByStartDate = sortBy("startDate");
+export const sortByMonthYear = sortBy("monthYear");
+export const sortByModifiedAt = sortBy("modifiedAt");
 
 export function getContractWithLastActivity({ shifts, contracts }) {
   if (shifts.length === 0) {
     if (contracts.length === 0) return "";
-    return sortObjects(contracts, "modified_at")[0].uuid;
+    return sortByModifiedAt(contracts)[0].uuid;
   } else if (shifts.length === 1) {
     return shifts[0].contract;
   } else {
-    return sortObjects(shifts, "modified_at")[0].contract;
+    return sortByModifiedAt(shifts)[0].contract;
   }
 }
 
