a919592e506cda445d2b3795332949382e19f24c
Sören Grieger <soeren.grieger@posteo.de>
2022-09-15 13:06:06 +0200

    calculating sufficient breaktimes inclusive detection for to long concatenated shifts, therefor created the method #getLastConcatenatedIndex and validate the sufficient breaktimes in the newShiftCutter class. The method calculateCuts got some fixing in the calculation. In ShiftForm although disabled to save overlapping shifts.

--- src/utils/shift.js
+++ src/utils/shift.js
@@ -39,11 +39,9 @@ export function getOverlappingShifts(shifts) {
   return matches;
 }
 
-export function sufficientBreaktimeBetweenShifts({ worktime, breaktime }) {
-  if (worktime > 9 * 60 && breaktime < 45) {
-    return false;
-  }
-  return !(worktime > 6 * 60 && breaktime < 30);
+export function sufficientBreaktimeBetweenShifts(shifts, newShift) {
+  let validator = new newShiftCutter(shifts, newShift);
+  return validator.validateSufficientBreaks();
 }
 
 export function missingBreaktime({ worktime, breaktime }) {
@@ -94,11 +92,76 @@ export class newShiftCutter {
     this.shifts = this.#privateSortShifts();
   }
 
+  #getLastConcatenatedIndex(startIndex, directionRight) {
+    if (directionRight) {
+      // lets go to the right
+      if (this.shifts.length === 1 || startIndex === this.shifts.length - 1)
+        return startIndex;
+      else {
+        for (let i = Number(startIndex); i < this.shifts.length - 1; i++) {
+          if (this.shifts[i].date.end !== this.shifts[i + 1].date.start)
+            return i;
+        }
+        return this.shifts.length - 1;
+      }
+    } else {
+      //lets go to the left
+      if (startIndex === 0 || this.shifts.length === 1) return startIndex;
+      else {
+        for (let i = Number(startIndex); i > 0; i--) {
+          if (this.shifts[i].date.start !== this.shifts[i - 1].date.end)
+            return i;
+        }
+        return 0;
+      }
+    }
+  }
+
   #privateSortShifts() {
     // todo: delete redundancy when refactor data filter is deployed
     return this.shifts.sort(
       (a, b) => parseISO(a.date.start) - parseISO(b.date.start)
     );
+  }
+
+  validateSufficientBreaks() {
+    let inTotalWorkBreakTime = coalescWorktimeAndBreaktime(this.shifts);
+    let newShiftIndex = this.shifts.findIndex(
+      (shift) => shift.uuid === this.new_shift_uuid
+    );
+    let leftSideWithNewShiftWorkBreak = coalescWorktimeAndBreaktime(
+      this.shifts.slice(0, newShiftIndex + 1)
+    );
+    let rightSideWithNewShiftWorkBreak = coalescWorktimeAndBreaktime(
+      this.shifts.slice(newShiftIndex, this.shifts.length)
+    );
+    let concatenatedRightShiftsWorkBreak = coalescWorktimeAndBreaktime(
+      this.shifts.slice(
+        newShiftIndex,
+        this.#getLastConcatenatedIndex(newShiftIndex, true) + 1
+      )
+    );
+    let concatenatedLeftShiftsWorkBreak = coalescWorktimeAndBreaktime(
+      this.shifts.slice(
+        this.#getLastConcatenatedIndex(newShiftIndex, false),
+        newShiftIndex + 1
+      )
+    );
+    if (
+      (inTotalWorkBreakTime.worktime > 9 * 60 &&
+        inTotalWorkBreakTime.breaktime < 45) ||
+      (inTotalWorkBreakTime.worktime > 6 * 60 &&
+        inTotalWorkBreakTime.worktime < 9 * 60 &&
+        inTotalWorkBreakTime.breaktime < 30) ||
+      (leftSideWithNewShiftWorkBreak.breaktime === 0 &&
+        leftSideWithNewShiftWorkBreak.worktime > 6 * 60) ||
+      (rightSideWithNewShiftWorkBreak.breaktime === 0 &&
+        rightSideWithNewShiftWorkBreak.worktime > 6 * 60) ||
+      concatenatedRightShiftsWorkBreak.worktime > 6 * 60 ||
+      concatenatedLeftShiftsWorkBreak.worktime > 6 * 60
+    )
+      return false;
+    return true;
   }
 
   calculateCuts() {
@@ -116,68 +179,100 @@ export class newShiftCutter {
     let rightOfNewShiftWorkBreakInMinutes = coalescWorktimeAndBreaktime(
       this.shifts.slice(newShiftIndex + 1, this.shifts.length)
     );
+    let leftSideWithNewShiftWorkBreak = coalescWorktimeAndBreaktime(
+      this.shifts.slice(0, newShiftIndex + 1)
+    );
+    let rightSideWithNewShiftWorkBreak = coalescWorktimeAndBreaktime(
+      this.shifts.slice(newShiftIndex, this.shifts.length)
+    );
+    let concatenatedRightShiftsWorkBreak = coalescWorktimeAndBreaktime(
+      this.shifts.slice(
+        newShiftIndex,
+        this.#getLastConcatenatedIndex(newShiftIndex, true) + 1
+      )
+    );
+    let concatenatedLeftShiftsWorkBreak = coalescWorktimeAndBreaktime(
+      this.shifts.slice(
+        this.#getLastConcatenatedIndex(newShiftIndex, false),
+        newShiftIndex + 1
+      )
+    );
 
     let inTotalWorkBreakTime = coalescWorktimeAndBreaktime(this.shifts);
 
     let newBreakInMinutes = 15;
+    // calculate new needed breaktime
+    if (inTotalWorkBreakTime.worktime > 540) {
+      newBreakInMinutes =
+        45 - inTotalWorkBreakTime.breaktime > 15
+          ? 45 - inTotalWorkBreakTime.breaktime
+          : 15;
+    } else if (inTotalWorkBreakTime.worktime > 360) {
+      newBreakInMinutes =
+        30 - inTotalWorkBreakTime.breaktime > 15
+          ? 30 - inTotalWorkBreakTime.breaktime
+          : 15;
+    }
     let newShiftLeftSideDuration = 0;
     let newShiftRightSideDuration = 0;
 
     if (
-      (inTotalWorkBreakTime.worktime > 540 &&
-        inTotalWorkBreakTime.breaktime > 45) ||
-      (inTotalWorkBreakTime.worktime > 360 &&
-        inTotalWorkBreakTime.breaktime > 30) ||
-      (leftOfNewShiftWorkBreakInMinutes.worktime === 0 &&
-        rightOfNewShiftWorkBreakInMinutes.worktime === 0 &&
-        this.shifts.length === 1)
+      leftOfNewShiftWorkBreakInMinutes.worktime === 0 &&
+      rightOfNewShiftWorkBreakInMinutes.worktime === 0 &&
+      this.shifts.length === 1
     ) {
-      // new shift > 6h and breaktime in total is already enough
+      // new shift > 6h and the only shift this day
       newShiftLeftSideDuration = 6 * 60;
       newShiftRightSideDuration =
         newShiftDuration - newBreakInMinutes - newShiftLeftSideDuration;
     } else {
-      // calculate new needed breaktime
-      if (inTotalWorkBreakTime.worktime > 540) {
-        newBreakInMinutes =
-          45 - inTotalWorkBreakTime.breaktime > 15
-            ? 45 - inTotalWorkBreakTime.breaktime
-            : 15;
-      } else if (inTotalWorkBreakTime.worktime > 360) {
-        newBreakInMinutes =
-          30 - inTotalWorkBreakTime.breaktime > 15
-            ? 30 - inTotalWorkBreakTime.breaktime
-            : 15;
-      }
-
       if (
-        rightOfNewShiftWorkBreakInMinutes.worktime >
-        leftOfNewShiftWorkBreakInMinutes.worktime
+        concatenatedRightShiftsWorkBreak.worktime > 6 * 60 ||
+        rightSideWithNewShiftWorkBreak.worktime > 6 * 60
       ) {
-        // right is more worktime
-
         newShiftRightSideDuration =
           360 - rightOfNewShiftWorkBreakInMinutes.worktime;
-        if (newShiftRightSideDuration + newBreakInMinutes >= newShiftDuration)
+        if (newShiftRightSideDuration + newBreakInMinutes > newShiftDuration)
           newShiftRightSideDuration = newShiftDuration - newBreakInMinutes;
         else {
           newShiftLeftSideDuration =
             newShiftDuration - newBreakInMinutes - newShiftRightSideDuration;
         }
-      } else {
-        // left is mor worktime OR both sides same worktime
+      }
+      if (
+        concatenatedLeftShiftsWorkBreak.worktime > 6 * 60 ||
+        leftSideWithNewShiftWorkBreak.worktime > 6 * 60
+      ) {
+        // left with new shift is more than 6h
 
         newShiftLeftSideDuration =
           360 - leftOfNewShiftWorkBreakInMinutes.worktime;
-        if (newShiftLeftSideDuration + newBreakInMinutes >= newShiftDuration)
-          newShiftLeftSideDuration = newShiftDuration - newBreakInMinutes;
-        else {
-          newShiftRightSideDuration =
-            newShiftDuration - newBreakInMinutes - newShiftLeftSideDuration;
+
+        if (newShiftRightSideDuration === 0) {
+          if (newShiftLeftSideDuration + newBreakInMinutes > newShiftDuration)
+            newShiftLeftSideDuration = newShiftDuration - newBreakInMinutes;
+          else {
+            newShiftRightSideDuration =
+              newShiftDuration - newBreakInMinutes - newShiftLeftSideDuration;
+          }
+        } else {
+          if (
+            newShiftLeftSideDuration +
+              newBreakInMinutes +
+              newShiftRightSideDuration >
+            newShiftDuration
+          )
+            newShiftLeftSideDuration =
+              newShiftDuration - newBreakInMinutes - newShiftRightSideDuration;
+          else {
+            newBreakInMinutes =
+              newShiftDuration -
+              newShiftRightSideDuration -
+              newShiftLeftSideDuration;
+          }
         }
       }
     }
-
     return {
       leftPartDuartion: newShiftLeftSideDuration,
       rightPartDuration: newShiftRightSideDuration,
