805fdf95c0c9b735a763779606125d19365d3852
Christian Grossmüller <chgad.games@gmail.com>
2022-09-28 11:13:03 +0200

    Refactor the Dialog, make it slim, make it reusable with a content slot which allows closing from inside the slot .

--- src/components/TheDialog.vue
+++ src/components/TheDialog.vue
@@ -2,18 +2,14 @@
   <v-dialog
     v-model="dialog"
     :persistent="persistent"
-    :max-width="maxWidth"
     :fullscreen="fullscreen"
-    transition="dialog-bottom-transition"
-    @click:outside="handleClickOutside"
-    @keydown.esc="$emit('close')"
-    @input="$emit('input', false)"
+    :max-width="maxWidth"
+    transition="slide-y-reverse-transition"
   >
     <template #activator="{ on }">
       <slot name="activator" :on="on"></slot>
     </template>
-
-    <slot name="content"></slot>
+    <slot name="content" :events="{ close }"></slot>
   </v-dialog>
 </template>
 
@@ -43,36 +39,9 @@ export default {
       dialog: false
     };
   },
-  watch: {
-    dialog: function (value) {
-      if (!value) return;
-
-      this.$emit("input", value);
-    },
-    value: function (value) {
-      if (value) return;
-
-      this.dialog = false;
-    }
-  },
-  created() {
-    const close = (e) => {
-      const ESC = 27;
-      if (e.keyCode !== ESC) return;
-      this.$emit("close");
-    };
-    // Close the modal when the
-    // user presses the ESC key.
-    document.addEventListener("keyup", close);
-    this.$on("hook:destroyed", () => {
-      document.removeEventListener("keyup", close);
-    });
-  },
   methods: {
-    handleClickOutside() {
-      if (this.persistent) return;
-
-      this.$emit("click:outside");
+    close() {
+      this.dialog = false;
     }
   }
 };
