86cac3c40d97b17d124df062bfb9f11b891e8ec9
Christian Grossmüller <chgad.games@gmail.com>
2022-10-04 17:57:45 +0200

    Create new component and implement it in ShiftFormFields.

--- src/components/forms/modelFroms/shift/ShiftFormFields.vue
+++ src/components/forms/modelFroms/shift/ShiftFormFields.vue
@@ -3,6 +3,10 @@
     <ShiftFormTags v-model="shift.tags" />
     <ShiftFormNote v-model="shift.note" />
     <ShiftFormType v-model="shift.type" />
+    <ShiftFormSelectContract
+      v-model="shift.contract"
+      :choices="validContracts"
+    />
   </v-card-text>
 </template>
 
@@ -11,9 +15,16 @@ import { Shift } from "@/models/ShiftModel";
 import ShiftFormNote from "@/components/shifts/ShiftFormNote";
 import ShiftFormTags from "@/components/shifts/ShiftFormTags";
 import ShiftFormType from "@/components/shifts/ShiftFormType";
+import ShiftFormSelectContract from "@/components/shifts/ShiftFormSelectContract";
+import { endOfDay, isWithinInterval, startOfDay } from "date-fns";
 export default {
   name: "ShiftFormFields",
-  components: { ShiftFormTags, ShiftFormNote, ShiftFormType },
+  components: {
+    ShiftFormTags,
+    ShiftFormNote,
+    ShiftFormType,
+    ShiftFormSelectContract
+  },
   props: {
     value: {
       type: Shift,
@@ -22,6 +33,20 @@ export default {
   },
   data() {
     return { shift: this.value };
+  },
+  computed: {
+    validContracts() {
+      console.log(this.shift);
+      return this.$store.getters["contentData/allContracts"].filter(
+        //TODO: Solve this with a mixin
+        (contract) => {
+          return isWithinInterval(this.shift.started, {
+            start: startOfDay(contract.startDate),
+            end: endOfDay(contract.endDate)
+          });
+        }
+      );
+    }
   },
   watch: {
     value(val) {
