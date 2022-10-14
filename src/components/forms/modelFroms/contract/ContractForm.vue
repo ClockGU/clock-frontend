97897bfc2a738148a039475848a05bb3c506ca19
Christian Grossmüller <chgad.games@gmail.com>
2022-10-14 17:29:13 +0200

    Add CardActions to ContractForm.

--- src/components/forms/modelFroms/contract/ContractForm.vue
+++ src/components/forms/modelFroms/contract/ContractForm.vue
@@ -6,16 +6,28 @@
       close-action
       @close="closeFn"
     ></CardToolbar>
+
+    <FormActions
+      :create-fn="saveContract"
+      :delete-fn="deleteContract"
+      :close-fn="closeFn"
+      :update-fn="updateContract"
+      :disable-save="false"
+      :is-new-instance="isNewInstance"
+      model-name="contract"
+    >
+    </FormActions>
   </v-card>
 </template>
 
 <script>
 import { Contract } from "@/models/ContractModel";
 import CardToolbar from "@/components/cards/CardToolbar";
+import FormActions from "@/components/cards/FormActions";
 
 export default {
   name: "ContractForm",
-  components: { CardToolbar },
+  components: { FormActions, CardToolbar },
   props: {
     existingContract: {
       type: [Contract, typeof undefined],
@@ -52,6 +64,15 @@ export default {
     this.initializeNewContract();
   },
   methods: {
+    saveContract() {
+      this.closeFn();
+    },
+    deleteContract() {
+      this.closeFn();
+    },
+    updateContract() {
+      this.close();
+    },
     initializeNewContract() {
       this.newContract =
         this.existingContract !== undefined
