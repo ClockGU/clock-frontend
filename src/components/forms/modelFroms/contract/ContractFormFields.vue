0b58ef4a2fd7f377e8bdaa9d48ca0d9e0aa270ce
Christian Grossmüller <chgad.games@gmail.com>
2022-10-14 17:41:28 +0200

    Create ContractFormFields.

---
+++ src/components/forms/modelFroms/contract/ContractFormFields.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card-text></v-card-text>
+</template>
+
+<script>
+import { Contract } from "@/models/ContractModel";
+
+export default {
+  name: "ContractFormFields",
+  props: {
+    value: {
+      type: Contract,
+      required: true
+    }
+  },
+  data() {
+    return {
+      contract: this.value
+    };
+  },
+  watch: {
+    value(value) {
+      this.contract = value;
+    },
+    contract(value) {
+      this.$emit("input", value);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
