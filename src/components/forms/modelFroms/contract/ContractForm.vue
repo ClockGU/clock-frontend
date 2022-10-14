db15921993e2130ef6e2b43918c5bdaaac7b3c02
Christian Grossmüller <chgad.games@gmail.com>
2022-10-14 17:23:05 +0200

    Add CardToolbar to ContractForm.

--- src/components/forms/modelFroms/contract/ContractForm.vue
+++ src/components/forms/modelFroms/contract/ContractForm.vue
@@ -1,12 +1,21 @@
 <template>
-  <v-card class="mx-auto word-break" min-width="400" :max-width="600"></v-card>
+  <v-card class="mx-auto word-break" min-width="400" :max-width="600">
+    <CardToolbar
+      :title="title"
+      :logout-action="false"
+      close-action
+      @close="closeFn"
+    ></CardToolbar>
+  </v-card>
 </template>
 
 <script>
 import { Contract } from "@/models/ContractModel";
+import CardToolbar from "@/components/cards/CardToolbar";
 
 export default {
   name: "ContractForm",
+  components: { CardToolbar },
   props: {
     existingContract: {
       type: [Contract, typeof undefined],
@@ -24,6 +33,21 @@ export default {
       newContract: undefined
     };
   },
+  computed: {
+    isNewInstance() {
+      return this.newContract.id === "";
+    },
+    title() {
+      if (this.isNewInstance) {
+        return this.$t("forms.titleAdd", {
+          entity: this.$tc("models.contract", 1)
+        });
+      }
+      return this.$t("forms.titleUpdate", {
+        entity: this.$tc("models.contract", 1)
+      });
+    }
+  },
   created() {
     this.initializeNewContract();
   },
@@ -33,6 +57,10 @@ export default {
         this.existingContract !== undefined
           ? this.existingContract.clone()
           : new Contract();
+    },
+    closeFn() {
+      this.initializeNewContract();
+      this.close();
     }
   }
 };
