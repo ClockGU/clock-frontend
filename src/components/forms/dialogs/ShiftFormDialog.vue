5db5c8ee4ef79b4756a9587265195ebfa379915b
Christian Grossmüller <chgad.games@gmail.com>
2022-10-27 14:49:21 +0200

    Implement ContractValidationMixn.

--- src/components/forms/dialogs/ShiftFormDialog.vue
+++ src/components/forms/dialogs/ShiftFormDialog.vue
@@ -5,7 +5,20 @@
     :persistent="false"
   >
     <template #activator="{ on }">
-      <v-btn v-on="on"> Open Dialog</v-btn>
+      <v-btn v-if="!icon" :color="btnColor" v-on="on">
+        {{
+          create
+            ? $t("buttons.newEntity", {
+                entity: $tc("models.shift")
+              })
+            : $t("buttons.updateEntity", {
+                entity: $tc("models.shift")
+              })
+        }}
+      </v-btn>
+      <v-btn v-else :color="btnColor" icon>
+        {{ create ? icons.mdiPlus : icons.mdiPencil }}</v-btn
+      >
     </template>
     <template #content="{ events: { close } }">
       <ShiftForm :existing-shift="shift" :close="close"></ShiftForm>
@@ -17,6 +30,7 @@
 import TheDialog from "@/components/TheDialog";
 import ShiftForm from "@/components/forms/modelFroms/shift/ShiftForm";
 import { Shift } from "@/models/ShiftModel";
+import { mdiPencil, mdiPlus } from "@mdi/js";
 export default {
   name: "ShiftFormDialog",
   components: { ShiftForm, TheDialog },
@@ -25,7 +39,27 @@ export default {
       type: Shift,
       required: false,
       default: undefined
+    },
+    create: {
+      type: Boolean,
+      default: false
+    },
+    icon: {
+      type: Boolean,
+      default: false
+    },
+    btnColor: {
+      type: String,
+      default: ""
     }
+  },
+  data() {
+    return {
+      icons: {
+        mdiPencil,
+        mdiPlus
+      }
+    };
   }
 };
 </script>
