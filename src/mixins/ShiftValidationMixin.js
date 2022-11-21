aa1e6721c24ade853e31365d88234106d3aa2984
Sören Grieger <soeren.grieger@posteo.de>
2022-11-21 10:51:29 +0100

    build validation for holidays

--- src/mixins/ShiftValidationMixin.js
+++ src/mixins/ShiftValidationMixin.js
@@ -1,31 +1,61 @@
+import Holidays from "date-holidays";
+
 export default {
-  data() {
-    return {
-      errorMessages: [],
-      alertMessages: []
-    };
-  },
   computed: {
     getAlertMessages() {
-      return this.alertMessages;
+      let alertMessages = [];
+      return alertMessages;
     },
     getErrorMessages() {
-      return this.errorMessages;
+      let errorMessages = [];
+      errorMessages.push(this.validateStartedBeforeStopped);
+      errorMessages.push(this.validateOnlyHolidayOnHolidays);
+      return errorMessages.filter((message) => message != undefined);
     },
     valid() {
       return this.errorMessages.length === 0;
     },
     validateStartedBeforeStopped() {
-      console.log("welcome to the first validation computed");
-      if (this.newShift.started > this.newShift.stopped) {
-        this.errorMessages.push(this.$t("shifts.errors.startedBeforeStopped"));
+      if (this.newShift != undefined) {
+        if (this.newShift.started > this.newShift.stopped) {
+          return this.$t("shifts.errors.startedBeforeStopped");
+        }
       }
     },
     validateMaxWorktimePerDay() {},
     validateNoSunday() {},
-    validateOnlyHolidayOnHolidays() {},
+    validateOnlyHolidayOnHolidays() {
+      if (this.newShift != undefined) {
+        if (this.dateIsHoliday & (this.newShift.type !== "bh")) {
+          return this.$t("shifts.errors.workingOnHolidays");
+        }
+      }
+    },
     validateExclusivityVacationAndSick() {},
     validateEightTwentyRule() {},
-    validateOverlapping() {}
+    validateOverlapping() {},
+    shiftsThisDay() {},
+    dateIsHoliday() {
+      // Christmas Eve and New Year's Eve are considered half Bank holidays
+      // by the date-holidays package.
+      // So we need to treat it separately
+      const date = this.newShift.started;
+
+      if (
+        date.getMonth() === 11 &&
+        (date.getDate() === 24 || date.getDate() === 31)
+      ) {
+        return true;
+      }
+      const hd = new Holidays("DE", "HE");
+      const holidayCandidate = hd.isHoliday(date); // returns false or array ... strange
+      if (!holidayCandidate) {
+        return holidayCandidate;
+      }
+      return (
+        holidayCandidate[0].type === "public" ||
+        holidayCandidate[0].type === "bank"
+      );
+    }
   }
 };
