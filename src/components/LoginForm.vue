cd360b00033a563c7c52358ee2d7fc9617cfb09e
Christian Grossmüller <chgad.games@gmail.com>
2023-01-16 10:20:49 +0100

    Fix vuelidation update in LoginForm.

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -26,7 +26,7 @@
           autocomplete="email"
           autofocus
           :error-messages="emailErrors"
-          @blur="$v.email.$touch()"
+          @blur="v$.email.$touch()"
           @keyup.enter="submit"
         ></v-text-field>
 
@@ -39,13 +39,17 @@
           :type="showPassword ? 'text' : 'password'"
           :append-icon="showPassword ? icons.mdiEye : icons.mdiEyeOff"
           :error-messages="passwordErrors"
-          @blur="$v.password.$touch()"
+          @blur="v$.password.$touch()"
           @keyup.enter="submit"
           @click:append="showPassword = !showPassword"
         ></v-text-field>
       </v-card-text>
       <v-card-actions>
-        <v-btn color="primary" :disabled="$v.$error" text @click.native="submit"
+        <v-btn
+          color="primary"
+          :disabled="v$.$error || !v$.$dirty"
+          text
+          @click.native="submit"
           >Login</v-btn
         >
       </v-card-actions>
@@ -54,17 +58,23 @@
 </template>
 
 <script>
-// import { validationMixin } from "vuelidate";
-// import { required, email } from "vuelidate/lib/validators";
+import { useVuelidate } from "@vuelidate/core";
 import { mdiAccount, mdiLock, mdiEye, mdiEyeOff } from "@mdi/js";
+import { required, email } from "@vuelidate/validators";
 
 export default {
   name: "LoginForm",
-  // mixins: [validationMixin],
-  // validations: {
-  //   email: { required, email },
-  //   password: { required }
-  // },
+  validations() {
+    return {
+      email: { required, email },
+      password: { required }
+    };
+  },
+  setup() {
+    return {
+      v$: useVuelidate()
+    };
+  },
   data: () => ({
     icons: {
       mdiAccount: mdiAccount,
@@ -80,22 +90,29 @@ export default {
   computed: {
     emailErrors() {
       const errors = [];
-      if (!this.$v.email.$dirty) return errors;
-      !this.$v.email.email && errors.push("Must be valid e-mail");
-      !this.$v.email.required && errors.push("E-mail is required");
+      if (!this.v$.email.$dirty) return errors;
+      if (this.v$.email.$errors.length > 0) {
+        for (let error of this.v$.email.$errors) {
+          errors.push(error.$message);
+        }
+      }
       return errors;
     },
     passwordErrors() {
       const errors = [];
-      if (!this.$v.password.$dirty) return errors;
-      !this.$v.password.required && errors.push("Password is required");
+      if (!this.v$.password.$dirty) return errors;
+      if (this.v$.password.$errors.length > 0) {
+        for (let error of this.v$.password.$errors) {
+          errors.push(error.$message);
+        }
+      }
       return errors;
     }
   },
   methods: {
     async submit() {
-      this.$v.$touch();
-      if (this.$v.$invalid) return;
+      this.v$.$touch();
+      if (this.v$.$invalid) return;
 
       try {
         this.loading = true;
