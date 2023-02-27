87399786914e5b149fabf99ac1bf3d4b0d8237c8
Christian Grossmüller <chgad.games@gmail.com>
2023-02-27 15:42:18 +0100

    Install vue-jest manually since it is no longer available in cli-plugin-unit-jest.

--- jest.config.js
+++ jest.config.js
@@ -171,7 +171,7 @@ module.exports = {
   moduleFileExtensions: ["js", "jsx", "json", "vue"],
 
   transform: {
-    "^.+\\.vue$": "vue-jest",
+    "^.+\\.vue$": "@vue/vue2-jest",
     ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
       "jest-transform-stub",
     "^.+\\.jsx?$": "babel-jest"
