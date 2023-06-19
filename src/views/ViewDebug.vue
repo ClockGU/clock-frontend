66c369d8b6cd61940cc7b990c8fb6234af46f037
Christian Grossmüller <chgad.games@gmail.com>
2023-06-19 11:06:26 +0200

    Refactor Short Shift functionality.

--- src/views/ViewDebug.vue
+++ src/views/ViewDebug.vue
@@ -54,6 +54,7 @@
 <script>
 import { subMinutes, subDays } from "date-fns";
 import SelectContractFilter from "@/components/SelectContractFilter";
+import { Shift } from "@/models/ShiftModel";
 
 export default {
   name: "ViewDebug",
@@ -76,15 +77,23 @@ export default {
       const contract = this.$store.getters["selectedContract/selectedContract"]
         .id;
 
-      const shift = { started, contract };
-
-      this.$store.dispatch("clock/CLOCK_SHIFT", { ...shift }).then(() => {
-        this.$store.dispatch("snackbar/setSnack", {
-          snack: "Debug: done.",
-          timeout: 4000,
-          color: "success"
-        });
+      const shift = new Shift({
+        started,
+        stopped: now,
+        contract,
+        wasReviewed: true
       });
+      this.$store
+        .dispatch("contentData/saveShift", shift.toPayload())
+        .then(() => {
+          this.$store.dispatch("snackbar/setSnack", {
+            message: "Debug: done.",
+            timePAssed: 0,
+            timeout: 4000,
+            color: "success",
+            show: true
+          });
+        });
     },
     overflowShift() {
       const now = new Date();
