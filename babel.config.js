356a9ba70a40b0a0c45b9ed73a62bdc310c1c5b1
Christian Grossmüller <chgad.games@gmail.com>
2023-07-07 12:29:44 +0200

    Make teste work with babel.

--- babel.config.js
+++ babel.config.js
@@ -4,9 +4,10 @@ module.exports = {
       plugins: [
         ["@babel/plugin-proposal-decorators", { legacy: true }],
         ["transform-require-context"]
-      ]
+      ],
+      presets: [["@vue/app"], ["@babel/preset-env"]]
     }
   },
-  plugins: ["@babel/plugin-proposal-decorators", { legacy: true }],
+  plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]],
   presets: [["@vue/app"], ["@babel/preset-env"]]
 };
