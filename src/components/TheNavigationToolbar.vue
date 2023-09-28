9a829538814f6bed47b6fe7ef2c02d6afa564059
Christian Grossmüller <chgad.games@gmail.com>
2023-09-28 19:51:08 +0200

    Migrate Navigation Drawer.

--- src/components/TheNavigationToolbar.vue
+++ src/components/TheNavigationToolbar.vue
@@ -1,16 +1,18 @@
 <template>
-  <v-toolbar class="mt-12 pt-4" max-height="10px" flat tag="nav">
-    <v-tabs align-tabs="center" optional>
-      <v-tab
-        v-for="item in links"
-        :key="item.text"
-        :to="item.to"
-        :ripple="false"
-      >
-        <v-icon start>{{ item.icon }}</v-icon>
-        {{ item.text }}
-      </v-tab>
-    </v-tabs>
+  <v-toolbar class="mt-12 pt-4" max-height="10px" flat tag="nav" :color="$vuetify.theme.current.colors.background">
+    <v-row justify="center">
+      <v-tabs align-tabs="center" optional>
+        <v-tab
+          v-for="item in links"
+          :key="item.text"
+          :to="item.to"
+          :ripple="false"
+        >
+          <v-icon start>{{ item.icon }}</v-icon>
+          {{ item.text }}
+        </v-tab>
+      </v-tabs>
+    </v-row>
   </v-toolbar>
 </template>
 
