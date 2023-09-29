adc257554e8c4ff09e7bd87761441abb89e31761
Christian Grossmüller <chgad.games@gmail.com>
2023-09-29 10:27:23 +0200

    Make NavigationToolbar Icons work.

--- src/components/TheNavigationToolbar.vue
+++ src/components/TheNavigationToolbar.vue
@@ -1,14 +1,14 @@
 <template>
   <v-toolbar class="mt-12 pt-4" max-height="10px" flat tag="nav" :color="$vuetify.theme.current.colors.background">
     <v-row justify="center">
-      <v-tabs align-tabs="center" optional>
+      <v-tabs optional>
         <v-tab
           v-for="item in links"
           :key="item.text"
           :to="item.to"
           :ripple="false"
         >
-          <v-icon start>{{ item.icon }}</v-icon>
+          <v-icon start :icon="item.icon"> </v-icon>
           {{ item.text }}
         </v-tab>
       </v-tabs>
