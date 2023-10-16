6dbcee2d73bc219669a1180c328be029ec2b4698
Christian Grossmüller <chgad.games@gmail.com>
2023-10-16 17:38:40 +0200

    Style Placholder according to old style.

--- src/components/Placeholder.vue
+++ src/components/Placeholder.vue
@@ -1,16 +1,23 @@
 <template>
-  <v-container fluid>
-    <v-row justify="center">
-      <component :is="component" height="200" />
+  <v-container class="px-0">
+    <v-row class="px-0">
+      <v-col cols="12">
+        <div class="d-flex align-content-center justify-center">
+          <component :is="component" height="200"/>
+        </div>
+      </v-col>
     </v-row>
-    <v-row justify="center" class="mt-6">
-      <p><slot></slot></p>
+    <v-row class="px-0" justify="center">
+      <v-col cols="12">
+        <p class="p-placeholder"><slot></slot></p>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
 import { UndrawFactory } from "@/factories/undrawFactory";
+import { markRaw } from "vue";
 
 export default {
   name: "Placeholder",
@@ -29,8 +36,18 @@ export default {
   }),
   created() {
     UndrawFactory.get(this.name).then((resolvedComponent) => {
-      this.component = resolvedComponent["default"];
+      this.component = markRaw(resolvedComponent["default"]);
     });
   }
 };
 </script>
+
+<style scoped>
+.p-placeholder {
+  font-size: .875rem;
+  line-height: 1.375rem;
+  color: rgb(0,0,0,.6);
+  font-weight: 400;
+  letter-spacing: .0071428571em;
+}
+</style>
