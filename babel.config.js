476c7a151214fdc24e65357388079814cd8fdd7e
Christian Grossmüller <chgad.games@gmail.com>
2024-02-26 18:02:33 +0100

    Prettyfy files not within src/ dir.

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,9 @@ module.exports = {
       presets: [["@vue/app"], ["@babel/preset-env"]]
     }
   },
-  plugins: [["@babel/plugin-proposal-decorators", { legacy: true }], ["@babel/plugin-syntax-import-attributes"]],
+  plugins: [
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-syntax-import-attributes"]
+  ],
   presets: [["@vue/app"], ["@babel/preset-env"]]
 };
