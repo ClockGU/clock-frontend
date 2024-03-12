8aac46288f90f025e2af46da8dd0d8ca229a7e50
Christian Grossmüller <chgad.games@gmail.com>
2024-03-12 15:40:55 +0100

    Try out different env files.

--- vite.config.js
+++ vite.config.js
@@ -1,79 +1,50 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import babel from "vite-plugin-babel";
 import eslint from "vite-plugin-eslint";
 import { configDefaults } from "vitest/config";
 
 const path = require("path");
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), "VITE");
-  throw Error(
-    JSON.stringify({
-      VITE_API_URL: env.VITE_API_URL,
-      VITE_PUBLIC_URL: env.VITE_PUBLIC_URL,
-      VITE_ALLOWED_HOST: env.VITE_ALLOWED_HOST,
-      VITE_I18N_LOCALE: env.VITE_I18N_LOCALE,
-      VITE_I18N_FALLBACK_LOCALE: env.VITE_I18N_FALLBACK_LOCALE,
-      VITE_MATOMO_URL: env.VITE_MATOMO_URL,
-      VITE_MATOMO_SIDE_ID: env.VITE_MATOMO_SIDE_ID,
-      VITE_MATOMO_DOMAINS: env.VITE_MATOMO_DOMAINS,
-      VITE_ENV: env.VITE_ENV,
-      VITE_LOCAL: env.VITE_LOCAL
-    })
-  );
-  return {
-    test: {
-      globals: true,
-      watch: false,
-      browser: {
-        enabled: true,
-        name: "firefox",
-        headless: true
-      },
-      exclude: [...configDefaults.exclude, "**/tests/e2e/**"]
+export default defineConfig({
+  test: {
+    globals: true,
+    watch: false,
+    browser: {
+      enabled: true,
+      name: "firefox",
+      headless: true
     },
-    plugins: [
-      vue(),
-      eslint(),
-      babel({
-        babelConfig: {
-          babelrc: false,
-          configFile: false,
-          plugins: [
-            [
-              "@babel/plugin-proposal-decorators",
-              { loose: true, version: "2022-03" }
-            ]
+    exclude: [...configDefaults.exclude, "**/tests/e2e/**"]
+  },
+  plugins: [
+    vue(),
+    eslint(),
+    babel({
+      babelConfig: {
+        babelrc: false,
+        configFile: false,
+        plugins: [
+          [
+            "@babel/plugin-proposal-decorators",
+            { loose: true, version: "2022-03" }
           ]
-        }
-      })
-    ],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src")
+        ]
       }
-    },
-    server: {
-      port: 8080,
-      hmr: {
-        clientPort: 443,
-        protocol: "wss"
-      }
-    },
-    preview: {
-      port: 8080
-    },
-    define: {
-      VITE_API_URL: env.VITE_API_URL,
-      VITE_PUBLIC_URL: env.VITE_PUBLIC_URL,
-      VITE_ALLOWED_HOST: env.VITE_ALLOWED_HOST,
-      VITE_I18N_LOCALE: env.VITE_I18N_LOCALE,
-      VITE_I18N_FALLBACK_LOCALE: env.VITE_I18N_FALLBACK_LOCALE,
-      VITE_MATOMO_URL: env.VITE_MATOMO_URL,
-      VITE_MATOMO_SIDE_ID: env.VITE_MATOMO_SIDE_ID,
-      VITE_MATOMO_DOMAINS: env.VITE_MATOMO_DOMAINS,
-      VITE_ENV: env.VITE_ENV,
-      VITE_LOCAL: env.VITE_LOCAL
+    })
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
     }
-  };
+  },
+  server: {
+    port: 8080,
+    hmr: {
+      clientPort: 443,
+      protocol: "wss"
+    }
+  },
+  preview: {
+    port: 8080
+  }
 });
