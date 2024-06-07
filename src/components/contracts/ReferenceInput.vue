9ad8b9e67fb712cabdb4c2ce558f2e27c82935fa
Christian Grossmüller <chgad.games@gmail.com>
2024-06-07 16:36:43 +0200

    Add ReferenceInput component.

--- src/components/contracts/ReferenceInput.vue
+++ src/components/contracts/ReferenceInput.vue
@@ -1,11 +1,26 @@
 <script setup>
+import {
+  mdiIdentifier,
+  mdiRotate3dVariant,
+  mdiInformationOutline
+} from "@mdi/js";
 
+const model = defineModel({ type: String });
 </script>
 
 <template>
-
+  <div class="d-flex">
+    <v-text-field
+      v-model="model"
+      :label="$t('contracts.reference')"
+      :prepend-icon="mdiIdentifier"
+      :hint="$t('contracts.referenceExplanation')"
+      persistent-hint
+      variant="filled"
+      required
+    />
+    <v-btn :icon="mdiRotate3dVariant" style="margin-inline: 8px"></v-btn>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
