6d3afc0880f98fdd520ad57f893d5ac444606e75
Christian Grossmüller <chgad.games@gmail.com>
2024-07-19 18:46:48 +0200

    Migrate ClockInOutCardForm to Composition API.

--- src/components/ClockInOutCardForm.vue
+++ src/components/ClockInOutCardForm.vue
@@ -1,15 +1,63 @@
+<script setup>
+import { mdiArrowLeft, mdiDelete } from "@mdi/js";
+import ShiftFormDialog from "@/components/forms/dialogs/ShiftFormDialog.vue";
+import { Shift } from "@/models/ShiftModel";
+import { ref } from "vue";
+import { useI18n } from "vue-i18n";
+
+const props = defineProps({
+  destroy: {
+    type: Function,
+    required: true
+  },
+  shift: {
+    type: Shift,
+    required: true
+  },
+  contractName: {
+    type: String,
+    required: true
+  },
+  overflow: {
+    type: Boolean,
+    default: false
+  }
+});
+
+defineEmits(["updateWindow"]);
+
+const dialog = ref(false);
+const { t } = useI18n(); // use as global scope
+
+function finish(saved) {
+  this.$emit("updateWindow", -1);
+  let message =
+    this.t("dashboard.clock.problems.messages.success") + " ";
+  if (saved) {
+    message += this.t("dashboard.clock.problems.messages.saved");
+  } else {
+    message += this.t("dashboard.clock.problems.messages.noSaved");
+  }
+  this.$store.dispatch("snackbar/setSnack", {
+    message: message,
+    color: "success"
+  });
+}
+
+</script>
+
 <template>
   <v-card>
     <v-toolbar :elevation="0">
       <v-btn icon @click="$emit('updateWindow', -1)">
-        <v-icon>{{ icons.mdiArrowLeft }}</v-icon>
+        <v-icon>{{ mdiArrowLeft }}</v-icon>
       </v-btn>
       <v-toolbar-title>
-        {{ $t("dashboard.clock.problems.title") }}
+        {{ t("dashboard.clock.problems.title") }}
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn icon @click="finish(false)">
-        <v-icon>{{ icons.mdiDelete }}</v-icon>
+        <v-icon>{{ mdiDelete }}</v-icon>
       </v-btn>
     </v-toolbar>
 
@@ -17,16 +65,16 @@
       <v-row justify="center">
         <div class="d-flex flex-column text-error">
           <div class="font-weight-bold">
-            {{ $tc("dashboard.clock.problems.text") }}
+            {{ t("dashboard.clock.problems.text") }}
           </div>
           <div class="font-weight-light">
-            {{ $tc("models.contract") }}: {{ contractName }}
+            {{ t("models.contract") }}: {{ contractName }}
           </div>
           <div v-if="overflow" class="font-weight-light">
-            {{ $t("dashboard.clock.problems.overflowedShift") }}
+            {{ t("dashboard.clock.problems.overflowedShift") }}
           </div>
           <div class="font-weight-light text-center">
-            {{ $tc("dashboard.clock.problems.editShift") }}
+            {{ t("dashboard.clock.problems.editShift") }}
           </div>
         </div>
       </v-row>
@@ -41,55 +89,3 @@
     </v-card-text>
   </v-card>
 </template>
-
-<script>
-import { mdiArrowLeft, mdiDelete } from "@mdi/js";
-import ShiftFormDialog from "@/components/forms/dialogs/ShiftFormDialog.vue";
-import { Shift } from "@/models/ShiftModel";
-
-export default {
-  name: "ClockInOutCardForm",
-  components: { ShiftFormDialog },
-  props: {
-    destroy: {
-      type: Function,
-      required: true
-    },
-    shift: {
-      type: Shift,
-      required: true
-    },
-    contractName: {
-      type: String,
-      required: true
-    },
-    overflow: {
-      type: Boolean,
-      default: false
-    }
-  },
-  emits: ["updateWindow"],
-  data: () => ({
-    dialog: false,
-    icons: { mdiArrowLeft, mdiDelete }
-  }),
-  methods: {
-    finish(saved) {
-      this.$emit("updateWindow", -1);
-      this.destroy(false).then(() => {
-        let message =
-          this.$t("dashboard.clock.problems.messages.success") + " ";
-        if (saved) {
-          message += this.$t("dashboard.clock.problems.messages.saved");
-        } else {
-          message += this.$t("dashboard.clock.problems.messages.noSaved");
-        }
-        this.$store.dispatch("snackbar/setSnack", {
-          message: message,
-          color: "success"
-        });
-      });
-    }
-  }
-};
-</script>
