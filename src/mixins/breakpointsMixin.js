2ae1886fb1b3700688209402f0ee6a9876e987bb
mlikayoussef <youssefmlika.lac2@gmail.com>
2025-01-03 11:05:51 +0100

    fixed some conflicts

--- src/mixins/breakpointsMixin.js
+++ src/mixins/breakpointsMixin.js
@@ -5,44 +5,12 @@ export default {
     },
     smAndDown() {
       return this.$vuetify.display.smAndDown;
+    },
+    isMobile() {
+      return this.$vuetify.display.mobile;
+    },
+    isXs() {
+      return this.$vuetify.display.xs;
     }
-
-    /*isXsOnly() {
-        return this.$vuetify.display.xsOnly;
-      },
-      smOnly() {
-        return this.$vuetify.display.smOnly;
-      },
-      mdOnly() {
-        return this.$vuetify.display.mdOnly;
-      },
-      lgOnly() {
-        return this.$vuetify.display.lgOnly;
-      },
-      xlOnly() {
-        return this.$vuetify.display.xlOnly;
-      },
-      xsAndUp() {
-        return this.$vuetify.display.xsAndUp;
-      },
-      
-      mdAndUp() {
-        return this.$vuetify.display.mdAndUp;
-      },
-      lgAndUp() {
-        return this.$vuetify.display.lgAndUp;
-      },
-      xlAndUp() {
-        return this.$vuetify.display.xlAndUp;
-      },
-      xsAndDown() {
-        return this.$vuetify.display.xsAndDown;
-      },
-      mdAndDown() {
-        return this.$vuetify.display.mdAndDown;
-      },
-      lgAndDown() {
-        return this.$vuetify.display.lgAndDown;
-      },*/
   }
 };
