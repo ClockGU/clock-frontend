cbce309a4f8777a5f7114facb4417a030efbfad6
CrypticCoder67458 <youssefmlika.lac2@gmail.com>
2025-10-02 10:29:19 +0200

    make ShiftListItem accessible by ensuring screen reader can reach it and adding aria-label describing shift

--- src/components/shifts/ShiftListItem.vue
+++ src/components/shifts/ShiftListItem.vue
@@ -1,17 +1,23 @@
 <template>
-  <v-list-item v-bind="$attrs">
-    <v-list-item-title :class="error ? 'error--text' : 'text--primary'">
-      <v-icon class="pr-1" :color="colors[shift.type]">
+  <v-list-item v-bind="$attrs" :aria-label="ariaLabel">
+    <v-list-item-title
+      :class="error ? 'error--text' : 'text--primary'"
+      aria-hidden="true"
+    >
+      <v-icon class="pr-1" :color="colors[shift.type]" aria-hidden="true">
         {{ typeIcons[shift.type] }}
       </v-icon>
       {{ formatDay(shift.started) }}
     </v-list-item-title>
-    <v-list-item-subtitle :class="error ? 'error--text' : 'text--primary'">
+    <v-list-item-subtitle
+      :class="error ? 'error--text' : 'text--primary'"
+      aria-hidden="true"
+    >
       {{ formatTime(shift.started) }} -
       {{ formatTime(shift.stopped) }}
       ({{ shift.representationalDuration("hm") }})
     </v-list-item-subtitle>
-    <v-list-item-subtitle>
+    <v-list-item-subtitle aria-hidden="true">
       <v-chip
         v-if="isRunningShift"
         class="ml-0"
@@ -95,6 +101,52 @@ export default {
         start: this.shift.started,
         end: this.shift.stopped
       });
+    },
+    ariaLabel() {
+      const day = this.formatDay(this.shift.started);
+      const startTime = this.formatTime(this.shift.started);
+      const stopTime = this.formatTime(this.shift.stopped);
+      const duration = this.shift.representationalDuration("hm");
+
+      const shiftTypeString = this.shift.type
+        ? this.$t(`aria.shift.${this.shift.type}`)
+        : "";
+      const dayString = day
+        ? this.$t("aria.shiftListItem.day", { day: day })
+        : "";
+      const startDateString = startTime
+        ? this.$t("aria.shiftListItem.startTime", { startTime: startTime })
+        : "";
+      const stopDateString = stopTime
+        ? this.$t("aria.shiftListItem.stopTime", { stopTime: stopTime })
+        : "";
+      const durationString = duration
+        ? this.$t("aria.shiftListItem.duration", { duration: duration })
+        : "";
+      const tagsKey = this.shift.tags.length
+        ? "aria.shiftListItem.tagsPrefix"
+        : "";
+      const reviewKey =
+        !this.shift.wasReviewed && !this.hideReviewedChip
+          ? "aria.shiftListItem.notReviewed"
+          : "";
+      const liveKey = this.isRunningShift ? "aria.shiftListItem.live" : "";
+      const tags = tagsKey
+        ? this.$t(tagsKey, { tags: this.shift.tags.join(", ") })
+        : "";
+      const reviewStatus = reviewKey ? this.$t(reviewKey) : "";
+      const liveStatus = liveKey ? this.$t(liveKey) : "";
+
+      const finalAriaLabel =
+        shiftTypeString +
+        dayString +
+        startDateString +
+        stopDateString +
+        durationString +
+        tags +
+        reviewStatus +
+        liveStatus;
+      return finalAriaLabel;
     }
   },
   methods: {
