2da224557d77566768e18c2f3ec63aea04e8c5d4
CrypticCoder67458 <youssefmlika.lac2@gmail.com>
2025-10-22 00:24:21 +0200

    fix small bug to calculate correct number of overlaps in DashboardConflicts

--- src/components/dashboard/DashboardConflicts.vue
+++ src/components/dashboard/DashboardConflicts.vue
@@ -98,7 +98,7 @@ export default {
 
     numberOfOverlaps() {
       if (this.disabled) return 0;
-      const overlaps = getOverlappingShifts(this.overlappingShifts).length;
+      const overlaps = getOverlappingShifts(this.allSelectedShifts).length;
       // use 0 case for clarity - the formula will evaluate to 1 on 0 overlaps
       const result = overlaps === 0 ? 0 : 0.5 + Math.sqrt(0.25 + 2 * overlaps);
       return Math.floor(result);
