20b3ce4ca2fd71ed0418f08dd5d3e6d518ae5a9d
CrypticCoder67458 <youssefmlika.lac2@gmail.com>
2025-11-09 21:13:50 +0100

    format

--- src/views/Reporting.vue
+++ src/views/Reporting.vue
@@ -211,8 +211,9 @@ export default {
         );
         let report = filteredReports[0];
         if (report === undefined) {
-          this.date =
-            this.selectedReports[this.selectedReports.length - 1].monthYear;
+          this.date = this.selectedReports[
+            this.selectedReports.length - 1
+          ].monthYear;
           return;
         }
         this.report = report;
